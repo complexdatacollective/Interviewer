e7066570601376808417a7efecbf41b38d231b33
Joshua Melville <joshmelville@gmail.com>
2014-09-14 21:12:47 +0100

    add and remove to bins

--- js/multibin.js
+++ js/multibin.js
@@ -27,6 +27,79 @@ var MultiBin = function MultiBin(options) {
     multiBin.destroy();
   };
 
+  var backgroundClickHandler = function(e) {
+    if (e.target !== e.currentTarget) {
+      // return false;
+      } else {
+      $('.container').fadeIn();
+      $('.copy').removeClass('node-bin-active'); 
+      setTimeout(function(){
+        $('.copy').remove();
+      },500);
+      
+
+      $('.content').off("click", backgroundClickHandler);      
+    }
+
+  };
+
+  var nodeBinClickHandler = function() {
+    if(!$(this).hasClass('.node-bin-active')) {
+      $('.content').on("click", backgroundClickHandler);
+      $('.container').fadeOut();
+      var position = $(this).offset();
+      var nodeBinDetails = $(this).clone(true);
+      nodeBinDetails.offset(position);
+      nodeBinDetails.addClass('copy');
+      nodeBinDetails.removeClass('node-bin-static');
+      nodeBinDetails.children('h1, h4').remove();
+
+      $('.content').append(nodeBinDetails);
+      $('.active-node-list').children('.node-item').css({top:0,left:20,opacity:0});
+
+      setTimeout(function(){
+        nodeBinDetails.addClass('node-bin-active');
+        $.each($('.active-node-list').children(), function(index,value) {
+          setTimeout(function(){
+            $(value).transition({left:0,opacity:1});
+          },50*index);
+        });
+      },0);
+    }
+
+  };
+
+  var nodeClickHandler = function() {
+    if($(this).parent().hasClass('active-node-list')) {
+      var el = $(this);
+      var id = $(this).parent().parent().data('index');
+      console.log('id is:');
+      console.log(id);
+      var edgeID = network.getEdges({from:network.getNodes({type_t0:'Ego'})[0].id,to:el.data('node-id'), type:'Dyad'})[0].id;
+      var properties = {};
+      properties[multiBin.options.variable.label] = '';
+      network.updateEdge(edgeID,properties);
+      var noun = "people";
+      console.log($('*[data-index="'+id+'"]').first().children('.active-node-list').children().length);
+      if ($('*[data-index="'+id+'"]').first().children('.active-node-list').children().length-1 === 1) {
+        noun = "person";
+      }
+      
+      if ($('*[data-index="'+id+'"]').first().children('.active-node-list').children().length-1 === 0) {
+      $('div[data-index="'+id+'"]').first().children('h4').html('(Empty)');
+    } else {
+      $('div[data-index="'+id+'"]').first().children('h4').html($('div[data-index="'+id+'"]').first().children('.active-node-list').children().length-1+' '+noun+'.');
+    }
+
+      $('div[data-index="'+id+'"]').first().children('.active-node-list').children().remove();
+      var parent = el.parent();  
+      el.detach().appendTo('.node-bucket');
+      var list = parent.children().clone(true);
+      $('div[data-index="'+id+'"]').first().children('.active-node-list').append(list);
+
+    }
+  };
+
   multiBin.destroy = function() {
     // Event Listeners
     notify("Destroying multiBin.",0);
@@ -37,6 +110,8 @@ var MultiBin = function MultiBin(options) {
   multiBin.init = function() {
     // Event Listeners
     window.addEventListener('changeStageStart', stageChangeHandler, false);
+    $(document).on("click", '.node-bin-static', nodeBinClickHandler);
+    $(document).on("click", '.node-item', nodeClickHandler);
 
     // Add header and subheader
     multiBin.options.targetEl.append('<h1>'+multiBin.options.heading+'</h1>');
@@ -50,7 +125,7 @@ var MultiBin = function MultiBin(options) {
 
     // Add edges to bucket
     $.each(edges, function(index,value) {
-      $('.node-bucket').append('<div class="node-item draggable">'+value.nname_t0+'</div>');
+      $('.node-bucket').append('<div class="node-item draggable" data-node-id="'+value.to+'">'+value.nname_t0+'</div>');
     });
 
 
@@ -62,28 +137,34 @@ var MultiBin = function MultiBin(options) {
     // One of these for each bin. One bin for each variable value.
 
     $.each(multiBin.options.variable.values, function(index, value){
-      var newBin = $('<div class="node-bin n'+index+'"><h1>'+value+'</h1><h4>(Empty)</h4></div>');
+      var newBin = $('<div class="node-bin node-bin-static n'+index+'" data-index="'+index+'"><h1>'+value+'</h1><h4>(Empty)</h4><div class="active-node-list"></div></div>');
+      newBin.data('index', index);
       multiBin.options.targetEl.append(newBin);
 
       $(".n"+index).droppable({ accept: ".draggable", 
         drop: function(event, ui) {
-          $(this).removeClass("over");
           var dropped = ui.draggable;
           var droppedOn = $(this);
-          $(dropped).detach().appendTo(droppedOn);
+          $(dropped).detach().appendTo(droppedOn.children('.active-node-list'));
+          var properties = {};
+          properties[multiBin.options.variable.label] = multiBin.options.variable.values[index];
+          // Add the attribute
+          var edgeID = network.getEdges({from:network.getNodes({type_t0:'Ego'})[0].id,to:$(dropped).data('node-id'), type:'Dyad'})[0].id;
+          network.updateEdge(edgeID,properties);
+          
           var noun = "people";
-          if ($(".n"+index).children().length-2 === 1) {
+          if ($(".n"+index+" .active-node-list").children().length === 1) {
             noun = "person";
           }
-          $(".n"+index+" h4").html($(".n"+index).children().length-2+' '+noun+'.');
+          $(".n"+index+" h4").html($(".n"+index+" .active-node-list").children().length+' '+noun+'.');
 
           var el = $(".n"+index);
             // var origBg = el.css('background-color');
-            el.transition({scale:1.2}, 400, 'ease');
+            el.transition({scale:1.2}, 200, 'ease');
             setTimeout(function(){
               el.transition({background:el.data('oldBg')}, 200, 'ease');
-              el.transition({ scale:1}, 500, 'ease');
-            }, 400);        
+              el.transition({ scale:1}, 200, 'ease');
+            }, 0);        
         }, 
         over: function() {
             $(this).data('oldBg', $(this).css('background-color'));
