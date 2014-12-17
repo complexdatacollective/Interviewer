248d348b147ceb8a334ba781675d60567ba7c931
Joshua Melville <joshmelville@gmail.com>
2014-12-17 20:22:06 +0000

    startup script

--- start.bat
+++ start.bat
@@ -1,5 +1,3 @@
-@start chrome --kiosk "http://localhost" --use-fake-ui-for-media-stream
-@start /b cmd /c java -version
-@start /b cmd /c gradle -version
-@start /b cmd /c groovy -version
-@start /b cmd /c grails -version
+start cmd /k ""C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath "C:\wamp\www\netCanvas\data\mongo""
+start cmd /k "node "c:\wamp\www\netCanvas\mongo-rest\express.js""
+start chrome --kiosk "http://localhost/netCanvas" --use-fake-ui-for-media-stream
