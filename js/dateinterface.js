ddb0daba69e1b7d836b9a14647909449cfd58ff9
Joshua Melville <joshmelville@gmail.com>
2015-01-21 04:53:47 +0000

    fixes #44

--- js/dateinterface.js
+++ js/dateinterface.js
@@ -49,6 +49,9 @@ var DateInterface = function DateInterface(options) {
 		                    '<input type="text" class="form-control" readonly />'+
 		                    '<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>'+
 		                '</div>'+
+                            '<div class="checkbox">'+
+                                '<label><input type="checkbox" name="checkbox-time" class="checkbox-time checkbox'+counter+'"> More than 6 months ago.</label>'+
+                            '</div>'+
 		            '</div>'+
 		        '</div>'+
 		        '<div class="col-sm-5 col-sm-offset-2">'+
@@ -58,6 +61,9 @@ var DateInterface = function DateInterface(options) {
 		                    '<input type="text" class="form-control" readonly />'+
 		                    '<span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>'+
 		                '</div>'+
+                            '<div class="checkbox">'+
+                                '<label><input type="checkbox" name="checkbox-time" class="checkbox-time checkbox'+(counter+1)+'"> More than 6 months ago.</label>'+
+                        '</div>'+
 		            '</div>'+
 		        '</div>'+
 		    '</div>'+
@@ -69,32 +75,65 @@ var DateInterface = function DateInterface(options) {
   		$('#datetimepicker'+counter).datetimepicker(dateoptions);
   		$('#datetimepicker'+(counter+1)).datetimepicker(dateoptions);
 
-        $(".row"+row).on("dp.change",function (e) {
-            console.log('change');
+        $('#datetimepicker'+counter+', #datetimepicker'+(counter+1)).on("dp.change",function (e) {
+            console.log('changed');
             var properties = {};
-            var target, first, second;
+            var target, first, second, incomingDate;
 
             var $current = $(this);
 
             if ($(this).hasClass('first')) {
 
+                if ($('.checkbox'+$current.attr('id').slice(-1)).is(':checked')) {
+                    console.log('checkbox for id '+$current.attr('id').slice(-1)+' is checked');
+                    properties.sex_first_before_range = true;
+                    incomingDate = null;
+                } else {
+                    console.log('checkbox for id '+$current.attr('id').slice(-1)+' is NOT checked');
+                    properties.sex_first_before_range = false;
+                    incomingDate = $current.data("DateTimePicker").date().format("MM/DD/YYYY");
+                }
+
                 target = parseInt($current.attr('id').slice(-1))+1;
                 first = parseInt($current.attr('id').slice(-1));
                 second = parseInt($current.attr('id').slice(-1))+1;
-                $('#datetimepicker'+target).data("DateTimePicker").minDate(e.date);
-                properties = {
-                    sex_first_t0: $current.data("DateTimePicker").date().format("MM/DD/YYYY")
-                };
+
+                if (e.date !== null ) {
+                    // $('#datetimepicker'+second).data("DateTimePicker").minDate(e.date);
+                }
+
+                properties.sex_first_t0 = incomingDate;
 
             } else {
+
+                if ($('.checkbox'+$current.attr('id').slice(-1)).is(':checked')) {
+                    console.log('checkbox for id '+$current.attr('id').slice(-1)+' is checked');
+                    properties.sex_last_before_range = true;
+                    incomingDate = null;
+                } else {
+                    console.log('checkbox for id '+$current.attr('id').slice(-1)+' is NOT checked');
+                    properties.sex_last_before_range = false;
+                    incomingDate = $current.data("DateTimePicker").date().format("MM/DD/YYYY");
+                }
+
                 target = parseInt($current.attr('id').slice(-1))-1;
                 first = parseInt($current.attr('id').slice(-1))-1;
                 second = parseInt($current.attr('id').slice(-1));
-                $('#datetimepicker'+target).data("DateTimePicker").minDate(e.date);
-                properties = {
-                    sex_last_t0: $current.data("DateTimePicker").date().format("MM/DD/YYYY")
-                };
+
+                if (e.date !== null) {
+                    // $('#datetimepicker'+first).data("DateTimePicker").maxDate(e.date);
+                }
+
+                properties.sex_last_t0 = incomingDate;
+
             }
+
+
+
+
+            console.log('about to do it');
+            console.log(properties);
+            network.updateEdge(value.id, properties);
 
             if (moment($('#datetimepicker'+first).data("DateTimePicker").date()).isAfter($('#datetimepicker'+second).data("DateTimePicker").date())) {
                 $current.parent().parent().parent().children('.logic-error').fadeIn();
@@ -104,23 +143,56 @@ var DateInterface = function DateInterface(options) {
                 $('.arrow-next').removeAttr('disabled');
             }
 
-            network.updateEdge(value.id, properties);
         });
 
         if (typeof value.sex_first_t0 !== 'undefined') {
             console.log('first:');
             console.log(value.sex_first_t0);
-            $('#datetimepicker'+counter).data("DateTimePicker").date(value.sex_first_t0);
+            if (value.sex_first_t0 === null) {
+                $('.checkbox'+counter).prop('checked', true);
+                $('#datetimepicker'+counter).data("DateTimePicker").date(moment().subtract(6, 'months').format("MM/DD/YYYY"));
+                $('#datetimepicker'+counter).children().css({opacity:0.5});
+                $('#datetimepicker'+counter).data("DateTimePicker").disable();
+
+            } else {
+                $('#datetimepicker'+counter).data("DateTimePicker").date(value.sex_first_t0);
+            }
+
         }
         if (typeof value.sex_last_t0 !== 'undefined') {
             console.log('last:');
             console.log(value.sex_last_t0);
-            $('#datetimepicker'+(counter+1)).data("DateTimePicker").date(value.sex_last_t0);
+            if (value.sex_last_t0 === null) {
+                $('.checkbox'+(counter+1)).prop('checked', true);
+                $('#datetimepicker'+(counter+1)).data("DateTimePicker").date(moment().subtract(6, 'months').format("MM/DD/YYYY"));
+                $('#datetimepicker'+(counter+1)).children().css({opacity:0.5});
+                $('#datetimepicker'+(counter+1)).data("DateTimePicker").disable();
+
+            } else {
+                $('#datetimepicker'+(counter+1)).data("DateTimePicker").date(value.sex_last_t0);
+            }
+
         }
+
+        $(".checkbox"+counter+', .checkbox'+(counter+1)).change(function(e) {
+            console.log('checkbox changed');
+            var $target = $(e.target);
+            if(this.checked) {
+                $target.parent().parent().parent().children('.date').data("DateTimePicker").date(moment().subtract(6, 'months').format("MM/DD/YYYY"));
+                $target.parent().parent().parent().children('.date').data("DateTimePicker").disable();
+                $target.parent().parent().parent().children('.date').children().css({opacity:0.5});
+            } else {
+                $target.parent().parent().parent().children('.date').data("DateTimePicker").enable();
+                $target.parent().parent().parent().children('.date').children().css({opacity:1});
+                $target.parent().parent().parent().children('.date').data("DateTimePicker").date(moment().format("MM/DD/YYYY"));
+            }
+        });
+
 
 		counter=counter+2;
         row++;
   	});
+
 
 
   };
