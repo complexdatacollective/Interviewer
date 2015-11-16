1fb2ebe5f22cbffec90b91fd904bf4294766a623
Joshua Melville <joshua.melville@oii.ox.ac.uk>
2015-11-16 21:43:31 +0000

    numerous namegenmulti improvements

--- js/namegeneratormulti.js
+++ js/namegeneratormulti.js
@@ -5,21 +5,20 @@ module.exports = function NameGeneratorMulti() {
     //global vars
     var nameGeneratorMulti = {};
     var options = {
-    	targetEl: $('.container'),
+        targetEl: $('.container'),
+    	panels: ['current'],
     	network: window.network,
     	form: window.forms.newNodeForm,
-    	heading: 'Within the last 6 months or so, who have you felt particularly close to, or discussed important personal matters with?',
-    	subheading: '',
-    	panels: [],
-    	dataOrigin: {
-    		type: 'node', // edge or node.
-    		variables: []
-    	},
-    	dataTarget: {
-    		type: 'node',
-    		variables: [
-    			{label: 'ng', value: 'close'},
-    			{label: 'contexts', value: []}
+    	data: {
+    		origin: 'node', // edge or node.
+    		namegenerators: [
+    			{
+    				prompt: 'Name generator prompt',
+    				label: 'name generator label',
+    				variables: [
+    					{label: 'extra-variable', value: 'true', type: 'select'}
+    				]
+    			}
     		]
     	}
     };
@@ -41,13 +40,13 @@ module.exports = function NameGeneratorMulti() {
 
         var node = options.network.getNode(index);
 
-        // add fields from dataTarget
+        // add fields from data
         var properties = {};
         properties.id = {
             type:'hidden',
             title: 'id'
         };
-        $.each(options.dataTarget.variables, function(targetIndex, targetValue) {
+        $.each(options.data.namegenerators, function(targetIndex, targetValue) {
             properties[targetValue.label] = {
                 type:'hidden',
                 title: targetValue.label
@@ -68,7 +67,7 @@ module.exports = function NameGeneratorMulti() {
             return false;
         }
 
-        var eachTime = 1000;
+        var eachTime = 1500;
 
         for (var i = 0; i < number; i++) {
             var timer = eachTime*i;
@@ -119,6 +118,12 @@ module.exports = function NameGeneratorMulti() {
         },
         {
             event: 'click',
+            handler: nameGeneratorMulti.toggleSelectable,
+            targetEl: window.document,
+            subTarget:  '.node-list-item'
+        },
+        {
+            event: 'click',
             handler: nameGeneratorMulti.showNewNodeForm,
             targetEl:  '.new-node-button'
         }];
@@ -157,8 +162,8 @@ module.exports = function NameGeneratorMulti() {
 
         // Prompts
         $('.question-container').append('<div class="swiper-container"><div class="swiper-wrapper"></div><div class="swiper-pagination"></div></div>');
-        for (var i = 0; i < options.dataOrigin.variables.length; i++) {
-            $('.swiper-wrapper').append('<div class="swiper-slide"><h2>'+options.dataOrigin.variables[i].prompt+'</h2></div>');
+        for (var i = 0; i < options.data.namegenerators.length; i++) {
+            $('.swiper-wrapper').append('<div class="swiper-slide"><h2>'+options.data.namegenerators[i].prompt+'</h2></div>');
         }
         promptSwiper = new Swiper ('.swiper-container', {
             pagination: '.swiper-pagination',
@@ -168,6 +173,7 @@ module.exports = function NameGeneratorMulti() {
         // Update current prompt counter
         promptSwiper.on('slideChangeStart', function () {
             currentPrompt = promptSwiper.activeIndex;
+            nameGeneratorMulti.handlePanels();
             nameGeneratorMulti.changeData();
         });
 
@@ -178,11 +184,10 @@ module.exports = function NameGeneratorMulti() {
 		// bin
 		options.targetEl.append('<div class="delete-bin-footer"><span class="delete-bin fa fa-4x fa-trash-o"></span></div>');
 		$('.delete-bin').droppable({
-			accept: '.card',
+			accept: '.card, .node-list-item',
 			tolerance: 'touch',
 			hoverClass: 'delete',
 			over: function( event, ui ) {
-                console.log('over');
 				$(this).addClass('delete');
 				$(ui.draggable).addClass('delete');
 			},
@@ -191,7 +196,6 @@ module.exports = function NameGeneratorMulti() {
 				$(ui.draggable).removeClass('delete');
 			},
 			drop: function( event, ui ) {
-                console.log(ui.draggable);
 				nameGeneratorMulti.removeNode($(ui.draggable).data('index'));
 			}
 		});
@@ -207,56 +211,25 @@ module.exports = function NameGeneratorMulti() {
           theme: 'default'
         });
 
-        nameGeneratorMulti.addExistingData();
         nameGeneratorMulti.handlePanels();
+        nameGeneratorMulti.addData();
         nameGeneratorMulti.bindEvents();
     };
 
     nameGeneratorMulti.changeData = function() {
-        if ($('.card').length > 0) {
-            $('.inner-card').removeClass('shown');
+            $('.inner-card, .node-list-item').removeClass('shown');
             setTimeout(function() {
-                $('.card').remove();
-                nameGeneratorMulti.addExistingData();
+                $('.card, .node-list-item').remove();
+                nameGeneratorMulti.addData();
             }, 1000);
-        } else {
-            nameGeneratorMulti.addExistingData();
-        }
-
     };
 
-    nameGeneratorMulti.updateSidePanel = function() {
-        // Empty it
-        $('.current-node-list').children().remove();
-
+    nameGeneratorMulti.getNodes = function(criteria) {
+        console.log('getnodes');
+        console.log(criteria);
+        var filterCriteria = criteria || {};
         // ignore ego and any nodes that are visible in the main node list
-        var nodes = options.network.getNodes({}, function (results) {
-            var filteredResults = [];
-            $.each(results, function(index,value) {
-                if (value.type !== 'Ego' && $('[data-index='+value.id+']').length === 0) {
-                    filteredResults.push(value);
-                }
-            });
-
-            return filteredResults;
-        });
-
-        $.each(nodes, function(index,value) {
-            var el = $('<div class="node-list-item">'+value.label+'</div>');
-            $('.current-node-list').append(el);
-        });
-
-    };
-
-    nameGeneratorMulti.addExistingData = function () {
-        var properties = {};
-        console.log(options);
-        console.log(currentPrompt);
-        // build properties array from dataOrigin
-        console.log(options.dataOrigin.variables[currentPrompt]);
-        properties[options.dataOrigin.variables[currentPrompt].label] = options.dataOrigin.variables[currentPrompt].value;
-
-        var nodes = options.network.getNodes(properties, function (results) {
+        var nodes = options.network.getNodes(filterCriteria, function (results) {
             var filteredResults = [];
             $.each(results, function(index,value) {
                 if (value.type !== 'Ego') {
@@ -267,17 +240,89 @@ module.exports = function NameGeneratorMulti() {
             return filteredResults;
         });
 
+        return nodes;
+    };
+
+    nameGeneratorMulti.toggleSelectable = function() {
+        var clicked = this;
+        var properties = {};
+
+        // get the togglePabelVariable for the current name generator
+        if (options.data.namegenerators[currentPrompt].togglePanelVariable !== 'undefined') {
+            $.each(options.data.namegenerators[currentPrompt].variables, function(variableIndex, variableValue) {
+                if (variableValue.label === options.data.namegenerators[currentPrompt].togglePanelVariable) {
+                    if ($(clicked).hasClass('selected')) {
+                        properties[options.data.namegenerators[currentPrompt].togglePanelVariable] = '';
+                    } else {
+                        properties[options.data.namegenerators[currentPrompt].togglePanelVariable] = variableValue.value;
+                    }
+                }
+            });
+
+            options.network.updateNode($(clicked).data('index'), properties, function() {
+                $(clicked).toggleClass('selected');
+            });
+        }
+
+    };
+
+    nameGeneratorMulti.updateSidePanel = function() {
+        console.log('updatesidepanel');
+
+        // Empty it
+        $('.current-node-list').children().remove();
+
+        // ignore ego and any nodes that are visible in the main node list
+        var nodes = nameGeneratorMulti.getNodes();
+
+        var filteredResults = [];
+        $.each(nodes, function(index,value) {
+            if (value.namegenerator !== options.data.namegenerators[currentPrompt].label) {
+                filteredResults.push(value);
+            }
+        });
+
+        $.each(filteredResults, function(index,value) {
+            var selected = '';
+
+            if (value[options.data.namegenerators[currentPrompt].togglePanelVariable] === 'true') {
+                selected = 'selected';
+            }
+
+            var el = $('<div class="node-list-item '+selected+'" data-index="'+value.id+'">'+value.label+'</div>');
+            $('.current-node-list').append(el);
+
+            setTimeout(function() {
+                $(el).addClass('shown');
+
+            },index*50);
+
+        });
+
+    };
+
+    nameGeneratorMulti.addData = function () {
+        console.log('add data');
+        var properties = {};
+        // build properties array from data
+        properties.namegenerator = options.data.namegenerators[currentPrompt].label;
+        console.log(properties);
+        var nodes = nameGeneratorMulti.getNodes(properties);
+        console.log(nodes);
         $.each(nodes, function(index,value) {
             setTimeout(function() {
                 nameGeneratorMulti.addCard(value);
             }, index * 100);
         });
 
+        nameGeneratorMulti.updateSidePanel();
+        nameGeneratorMulti.updateCounter();
+
     };
 
     nameGeneratorMulti.updateCounter = function(number) {
         if (!number) {
-            alterCounter.update(options.network.getNodes().length);
+            alterCounter.update(options.network.getNodes().length-1);
         } else {
             alterCounter.update(number);
         }
@@ -292,7 +337,6 @@ module.exports = function NameGeneratorMulti() {
             refreshPositions: true,
             scroll: false,
             start: function(event, ui) {
-                console.log(ui);
                 $(this).addClass('invisible');
                 $(ui.helper).addClass('dragging');
                 nameGeneratorMulti.showBin();
@@ -304,21 +348,62 @@ module.exports = function NameGeneratorMulti() {
             }
         });
 
+        $('.node-list-item').draggable({
+            // appendTo: 'body',
+            helper: 'clone',
+            revert: true,
+            revertDuration: 200,
+            refreshPositions: true,
+            scroll: false,
+            stack: '.node-list-item',
+            start: function(event, ui) {
+                nameGeneratorMulti.showBin();
+                $(ui.helper).addClass('dragging');
+            },
+            stop: function(event, ui) {
+                $(ui.helper).removeClass('dragging');
+                nameGeneratorMulti.hideBin();
+            }
+        });
+
     };
 
     nameGeneratorMulti.showNewNodeForm = function() {
 
-        // add fields from dataTarget
+        // add fields from data
         var properties = {};
-            properties[options.dataOrigin.variables[currentPrompt].label] = {
+            properties.namegenerator = {
                 type:'hidden',
-                title: options.dataOrigin.variables[currentPrompt].label
+                title: 'namegenerator'
             };
+
+        // Add additional variables, if present
+        if (typeof options.data.namegenerators[currentPrompt].variables !== 'undefined' && options.data.namegenerators[currentPrompt].variables.length > 0) {
+            $.each(options.data.namegenerators[currentPrompt].variables, function(variableIndex, variableValue) {
+
+                properties[variableValue.label] = {
+                    type: 'hidden',
+                    title: variableValue.label
+                };
+            });
+        }
+
         window.forms.nameGenForm.addTemporaryFields(properties);
 
         // Add data from fields
         properties = {};
-        properties[options.dataOrigin.variables[currentPrompt].label] = options.dataOrigin.variables[currentPrompt].value;
+        properties.namegenerator = options.data.namegenerators[currentPrompt].label;
+
+        // Add data to additional variables, if present
+        if (typeof options.data.namegenerators[currentPrompt].variables !== 'undefined' && options.data.namegenerators[currentPrompt].variables.length > 0) {
+
+            // Is there a cute way to do the below using map?
+            $.each(options.data.namegenerators[currentPrompt].variables, function(variableIndex, variableValue) {
+                properties[variableValue.label] = variableValue.value;
+            });
+        }
+
+
         window.forms.nameGenForm.addData(properties);
 
         window.forms.nameGenForm.show();
@@ -327,24 +412,54 @@ module.exports = function NameGeneratorMulti() {
     nameGeneratorMulti.handlePanels = function() {
         note.debug('nameGeneratorMulti.handlePanels()');
 
-            // Side container
-            var sideContainer = $('<div class="side-container"></div>');
+        if (options.panels.indexOf('current') !== -1) {
+            // We are trying to add a panel which shows the current nodes.
 
-            // Current side panel shows alters already elicited
-            sideContainer.append($('<div class="current-panel"><h4>People you already named:</h4><div class="current-node-list node-lists"></div></div>'));
+            // First, check there are some current nodes:
+            // ignore ego and any nodes that are visible in the main node list
+            var nodes = nameGeneratorMulti.getNodes();
 
-            if (sideContainer.children().length > 0) {
-                // move node list to one side
-                sideContainer.insertBefore('.nameList');
-                $('.nameList').addClass('alt');
+            var filteredResults = [];
+            $.each(nodes, function(index,value) {
+                if (value.namegenerator !== options.data.namegenerators[currentPrompt].label) {
+                    filteredResults.push(value);
+                }
+            });
+
+            if (filteredResults.length > 0) {
+                if ($('.side-container').length === 0) {
+                    // Side container
+                    var sideContainer = $('<div class="side-container out"></div>');
+
+                    // Current side panel shows alters already elicited
+                    sideContainer.append($('<div class="current-panel"><h4>Other people you have mentioned:</h4><div class="current-node-list node-lists"></div></div>'));
+
+                    if (sideContainer.children().length > 0) {
+                        // move node list to one side
+                        sideContainer.insertBefore('.nameList');
+                        setTimeout(function() {
+                            $('.nameList').addClass('alt');
+                            $('.side-container').removeClass('out');
+                        }, 10);
+
+
+                    }
+                }
+                // halve the panel height if we have two
+                if ($('.side-container').children().length > 1) {
+                    $('.node-lists').addClass('double');
+                }
+            } else {
+                $('.side-container').addClass('out');
+                $('.nameList').removeClass('alt');
+                setTimeout(function() {
+                    $('.side-container').remove();
+                }, 1000);
+
             }
 
-            nameGeneratorMulti.updateSidePanel();
+        }
 
-            // halve the panel height if we have two
-            if ($('.side-container').children().length > 1) {
-                $('.node-lists').addClass('double');
-            }
     };
 
     nameGeneratorMulti.showBin = function() {
@@ -369,7 +484,6 @@ module.exports = function NameGeneratorMulti() {
 
         nameGeneratorMulti.updateCounter();
         nameGeneratorMulti.makeDraggable();
-        nameGeneratorMulti.updateSidePanel();
 
         setTimeout(function() {
             $('[data-index='+properties.id+']').children('.inner-card').addClass('shown');
