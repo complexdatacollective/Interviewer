2bd924e1577da0480a13ebe5072eda3d0360c46d
Joshua Melville <joshmelville@gmail.com>
2016-05-06 10:03:29 +0100

    list select improvements

--- js/listselect_services.js
+++ js/listselect_services.js
@@ -16,14 +16,14 @@ module.exports = function ListSelect() {
 
         if ($(this).data('selected') === true) {
             $(this).data('selected', false);
-            $(this).css({'border':'2px solid #eee','background':'#eee'});
+            $(this).removeClass('selected');
 
             // remove values
             properties[listSelect.options.variable] = undefined;
 
         } else {
             $(this).data('selected', true);
-            $(this).css({'border':'2px solid red','background':'#E8C0C0'});
+            $(this).addClass('selected');
 
             // update values
 
@@ -59,22 +59,21 @@ module.exports = function ListSelect() {
         console.log(listSelect.options.criteria);
         $.each(edges, function(index,value) {
           var node = window.network.getNode(value.to);
-          var markup = ('<div class="collapse" id="collapse-'+value.id+'"><div class="well"><div class="btn-group big-check" data-toggle="buttons"></div></div>');
 
-          $.each(listSelect.options.variable.options, function(optionIndex, optionValue) {
-            console.log(optionValue);
-            // console.log($(markup).find('.big-check'));
-            $(markup).append('<label class="btn"><input type="radio" name="gender_k" data-value="Cisgender Male" checked><i class="fa fa-circle-o fa-3x"></i><i class="fa fa-check-circle-o fa-3x"></i> <span class="check-number">Cisgender Male</span></label>');
-          });
-
-          var el = $('<div class="item" data-toggle="collapse" data-target="#collapse-'+value.id+'" aria-expanded="false" aria-controls="collapse-'+value.id+'"><div class="inner" data-nodeid="'+value.id+'"><h3>'+node.name+'</h3>'+markup+'</div></div>');
-
+          var el = $('<div class="item" data-toggle="collapse" data-target="#collapse-'+value.id+'" aria-expanded="false" aria-controls="collapse-'+value.id+'"><div class="inner" data-nodeid="'+value.id+'"><h3>'+node.name+'</h3></div></div>');
 
             if (value[listSelect.options.variable] === 1) {
                 el.find('.inner').data('selected', true);
-                el.find('.inner').css({'border':'2px solid red','background':'#E8C0C0'});
+                el.find('.inner').addClass('selected');
             }
             $('.service-list-container').append(el);
+
+            var markup = ('<div class="collapse" id="collapse-'+value.id+'"><div class="well"><div class="btn-group big-check" data-toggle="buttons"></div></div>');
+            $('[data-nodeid="'+value.id+'"]').append(markup);
+
+            $.each(listSelect.options.variable.options, function(optionIndex, optionValue) {
+             $('[data-nodeid="'+value.id+'"]').find('.big-check').append('<label class="btn col-md-6"><input type="radio" name="gender_k" data-value="Cisgender Male" checked><i class="fa fa-circle-o fa-3x"></i><i class="fa fa-check-circle-o fa-3x"></i> <span class="check-number">'+optionValue+'</span></label>');
+            });
         });
 
         // Event Listeners
