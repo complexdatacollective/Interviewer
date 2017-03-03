53caf5856cb34951316bfa6b04c39a3fef740730
Joshua Melville <joshua.melville@northwestern.edu>
2017-03-03 23:05:10 +0000

    config.xml updates for cordova properties

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -12,8 +12,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-crosswalk-webview": "2.3.0",
+    "cordova-plugin-whitelist": "1.3.1",
     "cordova-plugin-wkwebview-engine": "1.1.1"
 };
 // BOTTOM OF METADATA
