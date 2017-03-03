5fb6cd6a54c7689eab434a81f0b532c24dedba46
Jamie Chung <jac@dropbox.com>
2017-03-03 08:33:53 -0800

    [WIP] Saving in progress.

--- src/components/ProtocolQuestion.js
+++ src/components/ProtocolQuestion.js
@@ -15,14 +15,46 @@ class ProtocolQuestion extends Component {
     if (this.props.protocolForm && this.props.protocolForm.values) {
       // TODO - skip logic using templating strings
       if (skip) {
-        // match the variable name in the parenthesis
-        const regEx = /\$\{([\s]*[^;\s\{]+[\s]*)\}/g;
-        const match = regEx.exec(skip);
+        // // match the variable name in the parenthesis
+        // const regEx = /\$\{([\s]*[^;\s\{]+[\s]*)\}/g;
+        // const match = regEx.exec(skip);
 
-        if (match !== null) {
-          console.log(match[1]);
-          console.log(this.props.protocolForm.values[match[1]]);
-        }
+        // if (match !== null) {
+        //   console.log(match[1]);
+        //   console.log(this.props.protocolForm.values[match[1]]);
+        // }
+
+        var generateTemplateString = (function(){
+            var cache = {};
+
+            function generateTemplate(template){
+
+            var fn = cache[template];
+
+            if (!fn){
+
+            // Replace ${expressions} (etc) with ${map.expressions}.
+
+            var sanitized = template
+                .replace(/\$\{([\s]*[^;\s]+[\s]*)\}/g, function(_, match){
+                    return `\$\{map.${match.trim()}\}`;
+                    })
+                // Afterwards, replace anything that's not ${map.expressions}' (etc) with a blank string.
+                .replace(/(\$\{(?!map\.)[^}]+\})/g, '');
+            console.log(sanitized);
+            fn = Function('map', `return \`'${sanitized}'\``);
+
+            }
+
+            return fn;
+        };
+
+        return generateTemplate;
+        })();
+
+        var formTemplate = generateTemplateString(skip);
+
+        console.log(formTemplate(this.props.protocolForm.values));
       }
 
 
