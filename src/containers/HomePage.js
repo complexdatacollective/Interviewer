db22b48b9932f10f85e09725436b4e9db7010ba2
Joshua Melville <joshua.melville@northwestern.edu>
2017-03-24 12:02:19 +0000

    remove react-icons for now

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import FaPlus from '../../node_modules/react-icons/fa/plus'
 import { actionCreators as networkActions } from '../ducks/modules/network';
 
 import { NodeForm } from './Forms';
@@ -33,7 +32,7 @@ class HomePage extends Component {
       <div className='grid__container'>
         <div className='grid__item grid--p-small'>
           <h1 className='type--home-title title--center'>
-            Welcome {participant.userProfile && participant.userProfile.name} <FaPlus />
+            Welcome {participant.userProfile && participant.userProfile.name}
           </h1>
           <h1 className='type--home-title title--center'>
             <Link to='protocol'>Access sample protocol</Link>
