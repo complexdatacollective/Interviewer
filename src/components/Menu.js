04e9d91419925131adfcdd187ad86760836bd20a
Joshua Melville <joshua.melville@northwestern.edu>
2017-03-28 20:03:30 +0100

    [wip] menu system

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,113 @@
-// Menu.js
+'use strict';
 
- import {scaleRotate as Menu} from 'react-burger-menu';
- import {decorator as reduxBurgerMenu} from 'redux-burger-menu';
+import React from 'react';
 
- export default reduxBurgerMenu(Menu);
+const MenuFactory = React.createClass({
+  propTypes: {
+    id: React.PropTypes.string,
+    isOpen: React.PropTypes.bool,
+  },
+
+  toggleMenu() {
+    console.log('toggleMenu ', this.state);
+
+    const newState = { isOpen: !this.state.isOpen };
+
+    console.log(newState);
+
+    this.setState(newState);
+  },
+
+  // Sets or unsets styles on DOM elements outside the menu component.
+  // This is necessary for correct page interaction with some of the menus.
+  // Throws and returns if the required external elements don't exist,
+  // which means any external page animations won't be applied.
+  componentWillUpdate(nextProps,nextState) {
+    console.log('handleExternalWrapper');
+
+    const wrapper = document.getElementById('page-wrap');
+
+    console.log(wrapper.className);
+    wrapper.className = nextState.isOpen ? 'isOpen' : '';
+
+  },
+
+  listenForClose(e) {
+    e = e || window.event;
+
+    if (this.state.isOpen && (e.key === 'Escape' || e.keyCode === 27)) {
+      this.toggleMenu();
+    }
+  },
+
+  getInitialState() {
+    const initialIsOpenProp = this.props && typeof this.props.isOpen !== 'undefined';
+    return { isOpen: initialIsOpenProp ? this.props.isOpen : false };
+  },
+
+  componentWillMount() {
+    // Allow initial open state to be set by props.
+    if (this.props.isOpen) {
+      this.toggleMenu();
+    }
+  },
+
+  componentDidMount() {
+    window.onkeydown = this.listenForClose;
+
+    // Allow initial open state to be set by props for animations with wrapper elements.
+    if (this.props.isOpen) {
+      this.toggleMenu();
+    }
+  },
+
+  componentWillUnmount() {
+    window.onkeydown = null;
+  },
+
+  componentWillReceiveProps(nextProps) {
+    if (typeof nextProps.isOpen !== 'undefined' && nextProps.isOpen !== this.state.isOpen) {
+      this.toggleMenu();
+    }
+  },
+
+  render() {
+    return (
+      <div>
+        <div className="bm-overlay" onClick={this.toggleMenu} />
+        <div id={this.props.id} className={this.state.isOpen ? 'bm-menu-wrap isOpen' : 'bm-menu-wrap'}>
+          <div className="bm-menu">
+            <div className="cross-icon">
+              <button onClick={this.toggleMenu} className="ui large button">
+                <i className="download icon"></i>
+                Cross
+              </button>
+            </div>
+            <header>
+              <h1>Stages</h1>
+            </header>
+            <nav className="bm-item-list">
+              {React.Children.map(this.props.children, (item, index) => {
+                if (item) {
+                  const extraProps = {
+                    key: index
+                  };
+                  return React.cloneElement(item, extraProps);
+                }
+              })}
+            </nav>
+          </div>
+        </div>
+        <div className="burger-icon">
+          <button onClick={this.toggleMenu} className="ui large button">
+            <i className="download icon"></i>
+            Burger
+          </button>
+        </div>
+      </div>
+    );
+  } // end render
+}); //end return
+
+
+export default MenuFactory;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,7 @@
 import React, { Component } from 'react';
 import { Menu } from '../components';
+import { Link } from 'react-router';
 require('../styles/main.scss');
-
-let MenuWrap = React.createClass({
-
-  getInitialState() {
-    return {hidden : false};
-  },
-
-  componentWillReceiveProps(nextProps) {
-    const sideChanged = this.props.children.props.right !== nextProps.children.props.right;
-
-    if (sideChanged) {
-      this.setState({hidden : true});
-
-      setTimeout(() => {
-        this.show();
-      }, this.props.wait);
-    }
-  },
-
-  show() {
-    this.setState({hidden : false});
-  },
-
-  render() {
-    let style;
-
-    if (this.state.hidden) {
-      style = {display: 'none'};
-    }
-
-    return (
-      <div style={style} className="right">
-        {this.props.children}
-      </div>
-    );
-  }
-});
 
 export default class App extends Component {
   render() {
@@ -51,12 +15,13 @@ export default class App extends Component {
 
     return (
       <div id='outer-container'>
-          <Menu pageWrapId={'page-wrap'} outerContainerId={'outer-container'} left>
-              <a id="home" className="menu-item" href="/">Home</a>
-              <a id="about" className="menu-item" href="/about">About</a>
-              <a id="contact" className="menu-item" href="/contact">Contact</a>
+          <Menu>
+            <Link to='protocol'>Access sample protocol</Link>
+            <Link to='protocol'>Access sample protocol</Link>
+            <Link to='protocol'>Access sample protocol</Link>
+            <Link to='protocol'>Access sample protocol</Link>
           </Menu>
-          <div id='page-wrap'>
+          <div id='page-wrap' className="">
             {children}
           </div>
       </div>
