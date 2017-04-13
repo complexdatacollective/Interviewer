35a099a491eb44f0026ce61b951e91314dd3e9f2
Stephen Mckellar <stephen@thingmaker.uk>
2017-04-13 16:39:33 +0100

    Detecting touch [wip]

--- src/components/InterfaceComponents/Prompt.js
+++ src/components/InterfaceComponents/Prompt.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import Touch from 'react-hammerjs';
 
 class Prompt extends Component {
+  constructor(props) {
+     super(props);
+
+     this.handleTap = this.handleTap.bind(this);
+     this.handleSwipe = this.handleSwipe.bind(this);
+   }
+
   currentPrompt() {
     const {
       prompts,
@@ -10,13 +18,29 @@ class Prompt extends Component {
     return prompts[currentIndex];
   }
 
+  handleSwipe(event) {
+    if(event.direction === 2) {
+      this.next();
+    }
+  }
+
+  handleTap() {
+    this.next();
+  }
+
+  next() {
+    alert('next');
+  }
+
   render() {
     const {
       title
     } = this.currentPrompt();
 
     return (
-      <div className='prompt'>Current prompt: { title }</div>
+      <Touch onTap={this.handleTap} onSwipe={this.handleSwipe} >
+        <div className='prompt'>Current prompt: { title }</div>
+      </Touch>
     );
   }
 }
