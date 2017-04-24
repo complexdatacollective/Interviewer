007bc47041441162935d29867bdc23383f9264e4
Stephen Mckellar <stephen@thingmaker.uk>
2017-04-24 11:07:41 +0100

    Test pips component

--- src/components/Elements/Pips.js
+++ src/components/Elements/Pips.js
@@ -8,17 +8,26 @@ class Pips extends Component {
       currentIndex,
     } = this.props;
 
+    let pips = [];
+
+    for(let index = 0; index < count; index++) {
+      const classes = (currentIndex === index ? 'pips__pip pips__pip--active' : 'pips__pip');
+      pips.push(<div key={ index } className={ classes }></div>);
+    };
+
     return (
       <div className='pips'>
         <div className='pips__pips'>
-          { [...Array(count).keys()].map((pip, index) => {
-            const classes = currentIndex === index ? 'pips__pip pips__pip--active' : 'pips__pip';
-            return <div key={ index } className={ classes }></div>;
-          }) }
+          { pips }
         </div>
       </div>
     );
   }
 }
+
+Pips.propTypes = {
+  count: React.PropTypes.number,
+  currentIndex: React.PropTypes.number,
+};
 
 export default Pips;
