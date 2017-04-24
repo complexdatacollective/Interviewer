0e9add4b54319cad329c40ecd43df862e12f56a1
Stephen Mckellar <stephen@thingmaker.uk>
2017-04-24 18:20:53 +0100

    Added snapshot tests for panels

---
+++ src/components/__tests__/Elements/Panel-test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Panel from '../../Elements/Panel';
+
+describe('Panel component', () => {
+  it('renders ok', () => {
+    const component = shallow(
+      <Panel title="foo">
+        <span>bar</span>
+      </Panel>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+});
---
+++ src/components/__tests__/Elements/Panels-test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Panels from '../../Elements/Panel';
+
+describe('Panels component', () => {
+  it('renders ok', () => {
+    const component = shallow(
+      <Panels>
+        <span>foo</span>
+      </Panels>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+
+});
