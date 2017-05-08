090cb5c777fad606a7d4f3d726f9e9708c76d5e2
Ronald Pedagat <ronline.pedagat@gmail.com>
2017-05-08 16:15:46 -0700

    add updater init to electron-starter

--- public/electron-starter.js
+++ public/electron-starter.js
@@ -7,6 +7,13 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+const updater = require('electron-simple-updater');
+
+updater.init({
+  checkUpdateOnStart: true,
+  autoDownload: true
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
