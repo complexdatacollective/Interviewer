a89ba493e8cff3f32c353e9d8b7c7d863dff0cf7
Steve Mckellar <steve@wwqrd.uk>
2017-05-10 11:18:44 +0100

    removed reference to scrollable

--- src/containers/Elements/index.js
+++ src/containers/Elements/index.js
@@ -2,4 +2,3 @@ export { default as NodeProvider } from './NodeProvider';
 export { default as NodeProviderPanels } from './NodeProviderPanels';
 export { default as PromptSwiper } from './PromptSwiper';
 export { default as Modal } from './Modal';
-export { default as Scrollable } from './Scrollable';
