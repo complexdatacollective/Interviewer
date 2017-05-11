bde80b55cf1cf73596bfc3bffd245c588ac160e7
Steve Mckellar <steve@wwqrd.uk>
2017-05-11 18:13:51 +0100

    Functional components

--- src/components/Elements/Panel.js
+++ src/components/Elements/Panel.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Panel extends Component {
+const Panel = (props) => {
+  const {
+    title,
+    children
+  } = props;
 
-  render() {
-    const {
-      title,
-      children
-    } = this.props;
-
-    return (
-      <div className='panel'>
-        <div className='panel__heading'><h3>{ title }</h3></div>
-        <div className='panel__content'>
-          { children }
-        </div>
+  return (
+    <div className='panel'>
+      <div className='panel__heading'><h3>{ title }</h3></div>
+      <div className='panel__content'>
+        { children }
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Panel;
