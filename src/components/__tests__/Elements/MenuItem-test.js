d36744110a6375135b8f4438991d46b2ea5cbc4b
Rebecca Madsen <rmadsen@hailmail.net>
2017-05-16 12:56:09 -0400

    linting

--- src/components/__tests__/Elements/MenuItem-test.js
+++ src/components/__tests__/Elements/MenuItem-test.js
@@ -1,29 +1,29 @@
 /* eslint-env jest */
 
 import React from 'react';
-import MenuItem from '../../Elements/MenuItem';
 import renderer from 'react-test-renderer';
+import MenuItem from '../../Elements/MenuItem';
 
-const toLink = "linkyLink";
+const toLink = 'linkyLink';
 const onClick = () => {};
-const imageType = "type";
-const title = "title";
+const imageType = 'type';
+const title = 'title';
 
 describe('MenuItem component', () => {
   it('renders menu item with title', () => {
     const component = renderer.create(
-      <MenuItem to={toLink} onClick={onClick} imageType={imageType} title={title} />
+      <MenuItem to={toLink} onClick={onClick} imageType={imageType} title={title} />,
     );
-    let tree = component.toJSON();
+    const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
   it('renders menu item with no image', () => {
     const component = renderer.create(
-      <MenuItem to={toLink} onClick={onClick} title={title} />
+      <MenuItem to={toLink} onClick={onClick} title={title} />,
     );
-    let tree = component.toJSON();
+    const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
   });
--- src/components/__tests__/Menu-test.js
+++ src/components/__tests__/Menu-test.js
@@ -1,8 +1,8 @@
 /* eslint-env jest */
 
 import React from 'react';
-import Menu from '../Menu';
 import renderer from 'react-test-renderer';
+import Menu from '../Menu';
 
 const items = [{},
           {},
@@ -11,12 +11,12 @@ const items = [{},
 describe('Menu component', () => {
   it('renders menu with list', () => {
     const component = renderer.create(
-      <Menu items={items} />
+      <Menu items={items} />,
     );
-    let tree = component.toJSON();
+    const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
   });
 
-  // TODO flush out tests, but the Menu is calling document.getElementById instead of using refs so some refactoring is needed.
+  // TODO flush out tests, but the Menu is calling document.getElementById instead of using refs
 });
