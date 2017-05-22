e962186f9556ab3d7184232c00ff96e2dd4cbddc
Rebecca Madsen <rmadsen@hailmail.net>
2017-05-22 12:01:35 -0400

    simplify scrollable?

--- src/behaviors/scrollable.js
+++ src/behaviors/scrollable.js
@@ -1,46 +1,14 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import { throttle } from 'lodash';
+import React from 'react';
 
 export default function scrollable(WrappedComponent) {
-  class Scrollable extends Component {
-
-    constructor() {
-      super();
-
-      this.state = { isScrolling: false };
-      this.updateScrollState = throttle(this.updateScrollState, 1000 / 24);  // 24 FPS
-    }
-
-    componentDidMount() {
-      if (this.node) {
-        this.node.addEventListener('scroll', this.updateScrollState);
-      }
-    }
-
-    componentWillUnmount() {
-      if (this.node) {
-        this.node.removeEventListener('scroll', this.updateScrollState);
-      }
-    }
-
-    updateScrollState = () => {
-      const isScrolling = this.node.scrollTop > 0;
-
-      this.setState({ isScrolling });
-    }
-
-    render() {
-      const classes = classNames('scrollable', { 'scrollable--is-scrolling': this.state.isScrolling });
-
-      return (
-        <div className={classes}>
-          <div className="scrollable__window" ref={(node) => { this.node = node; }}>
-            <WrappedComponent {...this.props} />
-          </div>
+  function Scrollable(props) {
+    return (
+      <div className="scrollable">
+        <div className="scrollable__window">
+          <WrappedComponent {...props} />
         </div>
-      );
-    }
+      </div>
+    );
   }
 
   return Scrollable;
