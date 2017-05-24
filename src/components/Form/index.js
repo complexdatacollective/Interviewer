5db1d5dad36167adb5b3123a718d6a396383c7fb
Steve Mckellar <steve@wwqrd.uk>
2017-05-24 16:18:13 +0100

    Fix Form/index

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-/* eslint-disable */
-=======
 /* eslint-disable import/prefer-default-export */
 
->>>>>>> 22575c1d4... Load basic config over http using rx
 export { default as TextInput } from './TextInput';
