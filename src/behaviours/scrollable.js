a7260a471be024687fe0fafba8081fd8a653ade9
Steve Mckellar <steve@wwqrd.uk>
2017-08-07 10:52:22 +0100

    Add scrollable detection back into scrollable behaviour. Issue #188

--- src/behaviours/scrollable.js
+++ src/behaviours/scrollable.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import { throttle } from 'lodash';
+
+const maxFramesPerSecond = 10;
 
 export default function scrollable(WrappedComponent) {
-  function Scrollable(props) {
-    return (
-      <div className="scrollable">
-        <WrappedComponent {...props} />
-      </div>
-    );
+  class Scrollable extends Component {
+    constructor() {
+      super();
+
+      this.state = { isScrolling: false };
+      this.updateScrollState = throttle(this.updateScrollState, 1000 / maxFramesPerSecond);
+    }
+
+    componentDidMount() {
+      this.scrollable.addEventListener('scroll', this.updateScrollState);
+    }
+
+    componentWillUnmount() {
+      this.scrollable.removeEventListener('scroll', this.updateScrollState);
+    }
+
+    updateScrollState = () => {
+      const isScrolling = this.scrollable.scrollTop > 0;
+
+      this.setState({
+        isScrolling,
+      });
+    }
+
+    render() {
+      const classes = classNames('scrollable', { 'scrollable--is-scrolling': this.state.isScrolling });
+
+      return (
+        <div className={classes} ref={(node) => { this.scrollable = node; }}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
   }
 
   return Scrollable;
