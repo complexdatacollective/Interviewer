5cb3af9a209fea95d1f791d4d8fb4c55291357a7
Steve Mckellar <steve@wwqrd.uk>
2017-08-11 11:13:19 +0100

    Draggable node bucket

--- src/containers/Elements/NodeBucket.js
+++ src/containers/Elements/NodeBucket.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { first } from 'lodash';
+import { Node } from 'network-canvas-ui';
+import { draggable } from '../../behaviours';
 
-const NodeBucket = () => (
-  <div className="node-bucket" />
+const EnhancedNode = draggable(Node);
+const label = node => node.name;
+
+const draggableType = 'NODE';
+
+const handleDropNode = (hits, node) => {
+  console.log(hits, node);
+};
+
+const NodeBucket = ({ node }) => (
+  <div className="node-bucket">
+    { node &&
+      <EnhancedNode
+        label={label(node)}
+        onDropped={hits => handleDropNode(hits, node)}
+        draggableType={draggableType}
+        {...node}
+      />
+    }
+  </div>
 );
 
-export default NodeBucket;
+NodeBucket.propTypes = {
+  node: PropTypes.object,
+};
+
+NodeBucket.defaultProps = {
+  node: null,
+};
+
+function mapStateToProps(state) {
+  return {
+    node: first(state.network.nodes),
+  };
+}
+
+function mapDispatchToProps() {
+  return {
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NodeBucket);
