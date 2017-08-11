1377f1a519cbc3283863f0b9dfb55bb850a3c71a
Steve Mckellar <steve@wwqrd.uk>
2017-08-11 11:30:13 +0100

    Added prompt to sociogram interface

--- src/containers/Interfaces/SociogramInterface.js
+++ src/containers/Interfaces/SociogramInterface.js
@@ -1,26 +1,39 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import {
   Sociogram,
   SociogramBackground,
   NodeLayout,
   EdgeLayout,
   NodeBucket,
+  PromptSwiper,
 } from '../Elements';
 
 /**
   * Sociogram Interface
   * @extends Component
   */
-const SociogramInterface = () => (
+const SociogramInterface = ({ config: { params: { prompts } } }) => (
   <div className="sociogram-interface">
-    <Sociogram>
-      <SociogramBackground />
-      <NodeLayout />
-      <EdgeLayout />
-      <NodeBucket />
-    </Sociogram>
+    <div className="sociogram-interface__prompts">
+      <PromptSwiper prompts={prompts} />
+    </div>
+    <div className="sociogram-interface__sociogram">
+      <div className="sociogram-interface__container">
+        <Sociogram>
+          <SociogramBackground />
+          <NodeLayout />
+          <EdgeLayout />
+          <NodeBucket />
+        </Sociogram>
+      </div>
+    </div>
   </div>
 );
+
+SociogramInterface.propTypes = {
+  config: PropTypes.object.isRequired,
+};
+
 
 export default SociogramInterface;
