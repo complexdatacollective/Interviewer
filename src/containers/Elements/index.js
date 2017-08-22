836e05d03817cf1cd1f765cdc5510287177906f5
Steve Mckellar <steve@wwqrd.uk>
2017-08-22 13:01:21 +0100

    HOC for modals

--- src/containers/Elements/index.js
+++ src/containers/Elements/index.js
@@ -1,7 +1,6 @@
 export { default as NodeProvider } from './NodeProvider';
 export { default as NodeProviderPanels } from './NodeProviderPanels';
 export { default as PromptSwiper } from './PromptSwiper';
-export { default as Modal } from './Modal';
 export { default as Dialog } from './Dialog';
 export { default as Form } from './Form';
 export { default as NodeForm } from './NodeForm';
