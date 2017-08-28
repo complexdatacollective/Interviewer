e632f46bdd5509857f4c29a03e9e9c375ff02de5
Rebecca Madsen <rmadsen@hailmail.net>
2017-08-28 13:44:19 -0400

    fix menu item double toggle

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -63,8 +63,8 @@ class MenuFactory extends Component {
 
   // intercepts click events; calls callback and toggles Menu open state
   menuItemClick = (itemClick) => {
+    this.menuClick();
     itemClick();
-    this.props.toggleMenu();
   }
 
   render() {
