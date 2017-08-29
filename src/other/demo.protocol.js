10f37f340df13930f0d420402bc853e8bf778a53
Joshua Melville <joshua.melville@northwestern.edu>
2017-08-29 12:11:11 +0100

    remove broken fields from new node form

--- src/other/demo.protocol.js
+++ src/other/demo.protocol.js
@@ -34,51 +34,6 @@ const fields = [
       maxValue: 200,
     },
   },
-  {
-    label: 'Which are your favourite package managers? (checkbox list)',
-    name: 'favourite_package_manager',
-    type: 'CheckboxGroup',
-    toggleComponent: 'checkbox',
-    options: ['yarn', 'npm', 'pip', 'gem', 'pear'],
-    validation: {
-      minSelected: 1,
-    },
-  },
-  {
-    label: 'Pick something from the network? (with selector)',
-    name: 'network_picker',
-    type: 'CheckboxGroup',
-    toggleComponent: 'context',
-    optionsSelector: (state) => (
-      state.network.nodes.map((node) => (node.name))
-    )
-  },
-  {
-    label: 'Which package managers have you used? (no validations)',
-    name: 'used_package_managers',
-    type: 'CheckboxGroup',
-    toggleComponent: 'checkbox',
-    options: ['yarn', 'npm', 'pip', 'gem', 'pear'],
-    validation: {
-    },
-  },
-  {
-    label: 'Pick one. (radio group)',
-    name: 'pick_opne',
-    type: 'RadioGroup',
-    options: ['yarn', 'npm', 'pip', 'gem', 'pear'],
-    validation: {
-    },
-  },
-  {
-    label: 'Which package managers have you contributed to? (toggle group)',
-    name: 'contributed_package_managers',
-    type: 'CheckboxGroup',
-    options: ['yarn', 'npm', 'pip', 'gem', 'pear'],
-    colors: ['neon-carrot', 'kiwi', 'sea-serpent', 'cerulean-blue', 'paradise-pink'],
-    validation: {
-    },
-  },
 ];
 export default {
   config: {
