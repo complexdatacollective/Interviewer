34500d2dbb303998bec7bd6af1653cfc4656eb04
Steve Mckellar <steve@wwqrd.uk>
2017-09-01 15:18:53 +0100

    bug/edit-modal-close

--- src/components/Elements/Modal.js
+++ src/components/Elements/Modal.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
-import React from 'react';
+import React, { Component } from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
 import { animation } from 'network-canvas-ui';
 import PropTypes from 'prop-types';
@@ -8,40 +8,57 @@ import modal from '../../behaviours/modal';
 /**
   * Renders a modal window.
   */
-const Modal = (props) => {
-  const {
-    show,
-    children,
-    title,
-    close,
-  } = props;
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.close = () => {};
+  }
 
-  return (
-    <CSSTransitionGroup
-      transitionName="modal--transition"
-      transitionEnterTimeout={animation.duration.standard}
-      transitionLeaveTimeout={animation.duration.standard}
-    >
-      { show &&
-        <div key="modal" className="modal" onClick={close}>
-          <div className="modal__window" onClick={e => e.stopPropagation()}>
-            <div className="modal__layout">
-              <div className="modal__layout-title">
-                <h1>{title}</h1>
+  // This is a work around for a bubbling touch event (something to do with our redux form)
+  componentDidUpdate() {
+    if (this.props.show) {
+      setTimeout(() => {
+        this.close = this.props.close;
+      }, 50);
+    } else {
+      this.close = () => {};
+    }
+  }
+
+  render() {
+    const {
+      show,
+      children,
+      title,
+    } = this.props;
+
+    return (
+      <CSSTransitionGroup
+        transitionName="modal--transition"
+        transitionEnterTimeout={animation.duration.standard}
+        transitionLeaveTimeout={animation.duration.standard}
+      >
+        { show &&
+          <div key="modal" className="modal" onClick={() => { this.close(); }}>
+            <div className="modal__window" onClick={e => e.stopPropagation()}>
+              <div className="modal__layout">
+                <div className="modal__layout-title">
+                  <h1>{title}</h1>
+                </div>
+                <div className="modal__layout-content">
+                  {children}
+                </div>
               </div>
-              <div className="modal__layout-content">
-                {children}
-              </div>
+              <button className="modal__close" onClick={() => { this.close(); }}>
+                Cancel
+              </button>
             </div>
-            <button className="modal__close" onClick={close}>
-              Cancel
-            </button>
           </div>
-        </div>
-      }
-    </CSSTransitionGroup>
-  );
-};
+        }
+      </CSSTransitionGroup>
+    );
+  }
+}
 
 Modal.propTypes = {
   close: PropTypes.func.isRequired,
