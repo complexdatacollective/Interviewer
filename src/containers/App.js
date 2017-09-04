4de68cc596acef821203d6ac774babc4ae54c46c
Joshua Melville <joshua.melville@northwestern.edu>
2017-09-04 11:26:24 +0100

    remove merge markup from setupscreen

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import cx from 'classnames';
 import { sessionMenuIsOpen, stageMenuIsOpen } from '../selectors/session';
 import { SessionMenu, StageMenu } from '.';
