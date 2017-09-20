32a1de2b412b560854873f96e906e90738954805
Steve Mckellar <steve@wwqrd.uk>
2017-09-20 16:40:45 +0100

    NodeLayout optimisations backed up with tests

--- src/containers/Elements/__tests__/NodeLayout-test.js
+++ src/containers/Elements/__tests__/NodeLayout-test.js
@@ -4,14 +4,18 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { NodeLayoutPure } from '../NodeLayout';
 
+const layout = 'foo';
+
 const mockProps = {
-  nodes: [],
+  nodes: [
+    { bar: 'buzz', uid: 123, [layout]: { x: 0, y: 0 } },
+  ],
   updateNode: () => {},
   toggleEdge: () => {},
   toggleNodeAttributes: () => {},
   width: 123,
   height: 456,
-  layout: 'foo',
+  layout,
 };
 
 describe('<NodeLayout />', () => {
@@ -19,5 +23,43 @@ describe('<NodeLayout />', () => {
     const component = shallow(<NodeLayoutPure {...mockProps} />);
 
     expect(component).toMatchSnapshot();
+  });
+
+  it('shouldComponentUpdate() only when prop `nodes` changes length', () => {
+    const componentDidUpdate = jest.fn();
+    NodeLayoutPure.prototype.componentDidUpdate = componentDidUpdate;
+
+    const component = shallow(
+      <NodeLayoutPure {...mockProps} />,
+      { lifecycleExperimental: true },
+    );
+
+    component.setProps({
+      nodes: [{ bar: 'buzz', uid: 123 }],
+    });
+
+    component.setProps({
+      nodes: [{ bar: 'buzz', uid: 123 }],
+    });
+
+    component.setProps({
+      nodes: [{ bar: 'buzz', uid: 123 }, { bar: 'bing', uid: 456 }],
+    });
+
+    expect(componentDidUpdate.mock.calls.length).toEqual(1);
+  });
+
+  it('should forceUpdate() when onDropped callback is called', () => {
+    const componentDidUpdate = jest.fn();
+    NodeLayoutPure.prototype.componentDidUpdate = componentDidUpdate;
+
+    const component = shallow(
+      <NodeLayoutPure {...mockProps} />,
+      { lifecycleExperimental: true },
+    );
+
+    component.find('Connect(LayoutNode)').at(0).prop('onDropped')();
+
+    expect(componentDidUpdate.mock.calls.length).toEqual(1);
   });
 });
