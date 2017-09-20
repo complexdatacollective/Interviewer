11771805d3243c729efe261a711ef74348f1ae28
Rebecca Madsen <rmadsen@hailmail.net>
2017-09-20 11:37:00 -0400

    move preventghostclick to selectable

--- src/behaviours/draggable.js
+++ src/behaviours/draggable.js
@@ -9,7 +9,6 @@ import { filter } from 'lodash';
 import DraggablePreview from '../utils/DraggablePreview';
 import { actionCreators as draggableActions } from '../ducks/modules/draggable';
 import { actionCreators as droppableActions } from '../ducks/modules/droppable';
-import PreventGhostClick from '../utils/PreventGhostClick';
 
 function isTouch(event) {
   if (typeof TouchEvent !== 'undefined' && event instanceof TouchEvent) {
@@ -76,7 +75,6 @@ export default function draggable(WrappedComponent) {
     }
 
     componentDidMount() {
-      PreventGhostClick(findDOMNode(this.node));
       this.el = findDOMNode(this.node);
       this.el.addEventListener('touchstart', this.handleMoveStart);
       this.el.addEventListener('touchmove', this.handleMove);
--- src/behaviours/selectable.js
+++ src/behaviours/selectable.js
@@ -1,16 +1,28 @@
-import React from 'react';
+/* eslint-disable react/no-find-dom-node */
+
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Touch from 'react-hammerjs';
+import { findDOMNode } from 'react-dom';
+import PreventGhostClick from '../utils/PreventGhostClick';
 
 export default function selectable(WrappedComponent) {
-  const Selectable = (props) => {
-    if (!props.canSelect) { return <WrappedComponent {...props} />; }
-    return (
-      <Touch onTap={props.onSelected}>
-        <WrappedComponent {...props} />
-      </Touch>
-    );
-  };
+  class Selectable extends Component {
+    componentDidMount() {
+      if (this.props.canSelect) {
+        PreventGhostClick(findDOMNode(this.node));
+      }
+    }
+
+    render() {
+      if (!this.props.canSelect) { return <WrappedComponent {...this.props} />; }
+      return (
+        <Touch onTap={this.props.onSelected} ref={(node) => { this.node = node; }}>
+          <WrappedComponent {...this.props} />
+        </Touch>
+      );
+    }
+  }
 
   Selectable.propTypes = {
     onSelected: PropTypes.func,
