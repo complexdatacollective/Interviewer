ea2fda3c5394bbbfcaebd7e44dcb0d44c0457d5b
Steve Mckellar <steve@wwqrd.uk>
2017-10-30 15:00:37 +0000

    Sociogram background with new protocol

--- src/containers/Elements/Sociogram.js
+++ src/containers/Elements/Sociogram.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
@@ -9,24 +11,26 @@ import {
 
 const Sociogram = ({ stage, prompt }) => (
   <div className="sociogram">
-    <SociogramBackground {...prompt.sociogram.background} />
-    {
-      prompt.sociogram.edge &&
-      <EdgeLayout
-        stage={stage}
-        prompt={prompt}
-      />
-    }
-    <NodeLayout
-      stage={stage}
-      prompt={prompt}
-    />
-    <NodeBucket
-      stage={stage}
-      prompt={prompt}
-    />
+    <SociogramBackground {...prompt.background} />
+
   </div>
 );
+
+// {
+//   prompt.sociogram.edge &&
+//   <EdgeLayout
+//     stage={stage}
+//     prompt={prompt}
+//   />
+// }
+// <NodeLayout
+//   stage={stage}
+//   prompt={prompt}
+// />
+// <NodeBucket
+//   stage={stage}
+//   prompt={prompt}
+// />
 
 Sociogram.propTypes = {
   stage: PropTypes.object.isRequired,
