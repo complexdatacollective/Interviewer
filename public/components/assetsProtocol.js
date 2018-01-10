09e56ead6e59c28098da2b4d72583621ba1bab51
Steve Mckellar <steve@wwqrd.uk>
2018-01-10 16:53:20 +0000

    Added asset protocol to Electron

---
+++ public/components/assetsProtocol.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const electron = require('electron')
+
+const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+
+const registerAssetsProtocol = () =>
+  electron.protocol.registerFileProtocol('asset', (request, callback) => {
+    const file = request.url.substr(8);
+    const filePath = path.normalize(path.join(userDataPath, file));
+
+    console.log(`REQUEST ASSET: ${filePath}`);
+
+    callback({ path: path.normalize(filePath) });
+
+  }, (error) => {
+    if (error) {
+      console.error('Failed to register protocol');
+    }
+  });
+
+exports.registerAssetsProtocol = registerAssetsProtocol;
