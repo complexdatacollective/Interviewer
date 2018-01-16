81c4ca30a42ef7e28fd7113d1a73f1f1051bfd64
Steve Mckellar <steve@wwqrd.uk>
2018-01-16 11:34:01 +0000

    Sketch filesystem/importer tests

---
+++ src/utils/__tests__/importer.test.js
@@ -0,0 +1,11 @@
+/* eslint-env jest */
+
+// import environments from '../../environments';
+// import importer from '../importer';
+
+describe('importer', () => {
+  describe('Electron', () => {
+    // const subject = importer(environments.ELECTRON);
+    it('copies the protocol files to the user data directory');
+  });
+});
