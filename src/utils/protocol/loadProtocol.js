a5b6b8061a785f67b3590448f8628ca9352c26f2
Steve Mckellar <steve@wwqrd.uk>
2018-02-06 12:10:25 +0000

    Tidy up loadProtocol

--- src/utils/protocol/loadProtocol.js
+++ src/utils/protocol/loadProtocol.js
@@ -1,10 +1,7 @@
-/* eslint-disable */
-
 import { readFile } from '../filesystem';
 import environments from '../environments';
 import inEnvironment from '../Environment';
 import protocolPath from './protocolPath';
-// import demoProtocol from '../../other/demo.canvas/protocol.json';
 
 const loadProtocol = (environment) => {
   if (environment !== environments.WEB) {
@@ -14,13 +11,9 @@ const loadProtocol = (environment) => {
   }
 
   if (environment === environments.WEB) {
-    return protocolName => {
-
-      fetch(`/protocols/${protocolName}/protocol.json`).then(response => console.log(response.json()));
-
-      return fetch(`/protocols/${protocolName}/protocol.json`)
+    return protocolName =>
+      fetch(`/protocols/${protocolName}/protocol.json`)
         .then(response => response.json());
-    }
   }
 
   throw Error(`loadProtocol not supported in this environment "${environment}"`);
