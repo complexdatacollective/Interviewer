33b9859384fcffd2e3b591c252f61cf0ec8774fe
Joshua Melville <joshua.melville@northwestern.edu>
2018-02-07 14:42:23 +0000

    revert babel transform

--- public/electron-starter.js
+++ public/electron-starter.js
@@ -1,32 +1,25 @@
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var electron = require('electron');
-var path = require('path');
-var url = require('url');
-var log = require('electron-log');
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const log = require('electron-log');
 require('./components/updater');
 
 // Module to control application life.
-var app = electron.app;
+const app = electron.app;
 // Module to create native browser window.
-var BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow;
 
-var isMacOS = function isMacOS() {
-  return require('os').platform() === 'darwin';
-};
+const isMacOS = () => require('os').platform() === 'darwin';
 
-var windowParameters = _extends({}, isMacOS() ? { titleBarStyle: 'hidden', frame: false } : {}, {
-  width: 1440,
-  height: 900,
-  center: true,
-  title: 'Network Canvas'
-});
+const titlebarParameters = isMacOS() ? { titleBarStyle: 'hidden', frame: false } : {};
+
+const windowParameters = Object.assign({ width: 1440, height: 900, center: true, title: 'Network Canvas' }, titlebarParameters);
 
 log.info('App starting...');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = void 0;
+let mainWindow;
 
 function createWindow() {
   // Create the browser window.
@@ -39,7 +32,7 @@ function createWindow() {
   }));
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -53,7 +46,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -61,11 +54,10 @@ app.on('window-all-closed', function () {
   }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
 });
-
