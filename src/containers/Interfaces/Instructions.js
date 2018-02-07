5b53740dcd03f32dfce1b412067d9fab2a3c7b58
Steve Mckellar <steve@wwqrd.uk>
2018-02-07 13:40:42 +0000

    Add Instructions interface

--- src/containers/Interfaces/Instructions.js
+++ src/containers/Interfaces/Instructions.js
@@ -2,15 +2,44 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { get, has } from 'lodash';
+import { Audio, Image, BackgroundImage } from '../../components';
+
+const renderItem = (item) => {
+  switch (item.type) {
+    case 'text':
+      // TODO: Do we really need it to contain html tags?
+      return <div dangerouslySetInnerHTML={{ __html: item.content }} />;
+    case 'image':
+      return <Image url={item.content} />;
+    case 'audio':
+      return <Audio url={item.content} controls autoPlay />;
+    default:
+      return null;
+  }
+};
+
+const renderItems = items =>
+  items.map((item, index) => (
+    <div className="instructions-interface__item" key={index}>
+      {renderItem(item)}
+    </div>
+  ));
+
 /**
   * Instruction Interface
   */
-const Instructions = ({ stage: { title, content} }) => (
-  <div className="instructions">
-    { title }
-  </div>
-);
+const Instructions = ({ stage: { title, items } }) => {
+  return (
+    <div className="interface instructions-interface">
+      <div className="instructions-interface__frame">
+        <h1 className="instructions-interface__title">{ title }</h1>
+        <div className="instructions-interface__items">
+          { renderItems(items) }
+        </div>
+      </div>
+    </div>
+  );
+};
 
 Instructions.propTypes = {
 };
