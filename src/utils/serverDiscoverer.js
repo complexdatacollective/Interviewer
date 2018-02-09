fe73bcfd2190a44b82313c5dcd3966c2f716bb56
Joshua Melville <joshua.melville@northwestern.edu>
2018-02-09 00:24:09 +0000

    working on electron

--- src/utils/serverDiscoverer.js
+++ src/utils/serverDiscoverer.js
@@ -4,38 +4,47 @@ import { isElectron, isCordova } from '../utils/Environment';
 
 class ServerDiscoverer {
   constructor() {
-    let EventEmitter;
-    let zeroconf;
-
-    // if (isElectron()) {
-    //   EventEmitter = window.require('eventemitter3');
-    //   diont = window.require('diont')();
-    // }
-
-    if (isCordova()) {
-      EventEmitter = require('eventemitter3');
-      zeroconf = window.cordova.plugins.zeroconf;
-      zeroconf.watchAddressFamily = 'ipv4';
-    }
-
     if (!isElectron() && !isCordova()) {
       return { on: () => { /* noop */ } };
     }
+
+    const EventEmitter = window.require('eventemitter3');
     this.events = new EventEmitter();
-    zeroconf.watch('_network-canvas._tcp.', 'local.', (result) => {
-      const action = result.action;
-      const service = result.service;
-      if (action === 'added') {
-        console.log('service added', service);
+
+    if (isElectron()) {
+      const mdns = window.require('mdns');
+
+      const browser = mdns.createBrowser({ name: 'network-canvas', protocol: 'tcp' });
+      browser.on('serviceUp', (service) => {
         this.events.emit('SERVICE_ANNOUNCED', service);
-      } else if (action === 'resolved') {
-        console.log('service resolved', service);
-        this.events.emit('SERVICE_RESOLVED', service);
-      } else {
-        console.log('service removed', service);
+        console.log(service);
+      });
+      browser.on('serviceDown', (service) => {
         this.events.emit('SERVICE_REMOVED', service);
-      }
-    });
+        console.log(service);
+      });
+      browser.start();
+    }
+
+    if (isCordova()) {
+      const zeroconf = window.cordova.plugins.zeroconf;
+      zeroconf.watchAddressFamily = 'ipv4';
+
+      zeroconf.watch('_network-canvas._tcp.', 'local.', (result) => {
+        const action = result.action;
+        const service = result.service;
+        if (action === 'added') {
+          console.log('service added', service);
+          this.events.emit('SERVICE_ANNOUNCED', service);
+        } else if (action === 'resolved') {
+          console.log('service resolved', service);
+          this.events.emit('SERVICE_RESOLVED', service);
+        } else {
+          console.log('service removed', service);
+          this.events.emit('SERVICE_REMOVED', service);
+        }
+      });
+    }
   }
 
   on(...args) {
