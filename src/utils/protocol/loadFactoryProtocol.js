fe05793706b6687e747258f1600ba31b54a2c758
Steve Mckellar <steve@wwqrd.uk>
2018-02-12 15:45:30 +0000

    Add load protocol errors

--- src/utils/protocol/loadFactoryProtocol.js
+++ src/utils/protocol/loadFactoryProtocol.js
@@ -2,18 +2,23 @@ import { readFile } from '../filesystem';
 import environments from '../environments';
 import inEnvironment from '../Environment';
 import factoryProtocolPath from './factoryProtocolPath';
+import { friendlyErrorMessage } from '../../ducks/modules/errors';
 
 const loadFactoryProtocol = (environment) => {
+  const readProtocolError = friendlyErrorMessage("We couldn't read the protocol. The file may be broken, please try importing it again.");
+
   if (environment !== environments.WEB) {
     return protocolName =>
       readFile(factoryProtocolPath(protocolName, 'protocol.json'))
-        .then(data => JSON.parse(data));
+        .then(data => JSON.parse(data))
+        .catch(readProtocolError);
   }
 
   if (environment === environments.WEB) {
     return protocolName =>
       fetch(`/protocols/${protocolName}/protocol.json`)
-        .then(response => response.json());
+        .then(response => response.json())
+        .catch(readProtocolError);
   }
 
   throw Error(`loadFactoryProtocol not supported in this environment "${environment}"`);
--- src/utils/protocol/loadProtocol.js
+++ src/utils/protocol/loadProtocol.js
@@ -2,12 +2,14 @@ import { readFile } from '../filesystem';
 import environments from '../environments';
 import inEnvironment from '../Environment';
 import protocolPath from './protocolPath';
+import { friendlyErrorMessage } from '../../ducks/modules/errors';
 
 const loadProtocol = (environment) => {
   if (environment !== environments.WEB) {
     return protocolName =>
       readFile(protocolPath(protocolName, 'protocol.json'))
-        .then(data => JSON.parse(data));
+        .then(data => JSON.parse(data))
+        .catch(friendlyErrorMessage("We couldn't read the protocol. The file may be broken, please try importing it again."));
   }
 
   throw Error(`loadProtocol not supported in this environment "${environment}"`);
