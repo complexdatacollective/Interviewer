25485a110e28dfdab30497175ef60c3fa9af1bd5
Steve Mckellar <steve@wwqrd.uk>
2018-02-12 16:06:56 +0000

    Remove page reducer

--- src/ducks/modules/rootReducer.js
+++ src/ducks/modules/rootReducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import network from './network';
-import page from './page';
 import participant from './participant';
 import protocol from './protocol';
 import session from './session';
@@ -16,7 +15,6 @@ import search from './search';
 const appReducer = combineReducers({
   form: formReducer,
   network,
-  page,
   participant,
   protocol,
   session,
