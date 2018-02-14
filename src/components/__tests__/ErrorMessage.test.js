aa0f62379ae348a1d1cf9e31b8a102cdd39c1033
Steve Mckellar <steve@wwqrd.uk>
2018-02-14 16:27:30 +0000

    Updated ErrorMessage tests, and introduced Dialog tests

--- src/components/__tests__/ErrorMessage.test.js
+++ src/components/__tests__/ErrorMessage.test.js
@@ -4,18 +4,53 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { ErrorMessage } from '../ErrorMessage';
 
+jest.mock('../../ducks/modules/protocol');
+
 const mockProps = {
-  errorMessage: 'There was an error',
-  acknowledged: false,
+  error: null,
+  acknowledged: true,
   acknowledgeError: () => {},
 };
 
 describe('<ErrorMessage />', () => {
-  it('renders ok', () => {
+  it('can render', () => {
     const component = shallow(
       <ErrorMessage {...mockProps} />,
     );
 
     expect(component).toMatchSnapshot();
   });
+
+  describe('show', () => {
+    const testProps = {
+      ...mockProps,
+      error: new Error('there was an error'),
+      acknowledged: false,
+    };
+
+    it('when error and acknowledge, show is true', () => {
+      const component = shallow(
+        <ErrorMessage {...testProps} />,
+      );
+
+      expect(component.find('Dialog').prop('show')).toEqual(true);
+    });
+    it('when no error, show is false', () => {
+      const component = shallow(
+        <ErrorMessage {...{ ...testProps, error: null }} />,
+      );
+
+      expect(component.find('Dialog').prop('show')).toEqual(false);
+    });
+
+    it('when acknowleged, show is false', () => {
+      const component = shallow(
+        <ErrorMessage {...{ ...testProps, acknowledged: true }} />,
+      );
+
+      expect(component.find('Dialog').prop('show')).toEqual(false);
+    });
+  });
+
+
 });
