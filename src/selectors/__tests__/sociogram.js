1ab50fcbaf2083e38b46711e3bca571a279a0f91
Steve Mckellar <steve@wwqrd.uk>
2018-02-14 11:46:51 +0000

    Test other allow highlighting options in sociogram selector.

--- src/selectors/__tests__/sociogram.js
+++ src/selectors/__tests__/sociogram.js
@@ -2,10 +2,12 @@
 
 import { makeGetSociogramOptions } from '../sociogram';
 
+const mockPrompt = {
+  layout: {},
+};
+
 const mockProps = {
-  prompt: {
-    layout: {},
-  },
+  prompt: mockPrompt,
 };
 
 const mockState = {
@@ -19,9 +21,34 @@ describe('sociogram selector', () => {
       getSociogramOptions = makeGetSociogramOptions();
     });
 
-    it('when highlight option is missing, allowHighlighting is false', () => {
-      const subject = getSociogramOptions(mockState, mockProps);
-      expect(subject.allowHighlighting).toEqual(false);
+    describe('props.prompt.highlight', () => {
+      it('when highlight option is missing, allowHighlighting is false', () => {
+        const subject = getSociogramOptions(mockState, mockProps);
+        expect(subject.allowHighlighting).toEqual(false);
+      });
+      it('when highlight option exists, allowHighlighting is true', () => {
+        const prompt = {
+          ...mockPrompt,
+          highlight: {},
+        };
+        const subject = getSociogramOptions(mockState, { prompt });
+        expect(subject.allowHighlighting).toEqual(true);
+      });
+      it('when allowHighlighting option exists, allowHighlighting should match', () => {
+        let prompt = {
+          ...mockPrompt,
+          highlight: { allowHighlighting: false },
+        };
+        let subject = getSociogramOptions(mockState, { prompt });
+        expect(subject.allowHighlighting).toEqual(false);
+
+        prompt = {
+          ...mockPrompt,
+          highlight: { allowHighlighting: true },
+        };
+        subject = getSociogramOptions(mockState, { prompt });
+        expect(subject.allowHighlighting).toEqual(true);
+      });
     });
   });
 });
