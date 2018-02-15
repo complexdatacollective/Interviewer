f8991f0b7498c16629221af3c197a6d59c3efa32
Steve Mckellar <steve@wwqrd.uk>
2018-02-15 13:30:23 +0000

    Add reducers for server events

--- src/ducks/modules/servers.js
+++ src/ducks/modules/servers.js
@@ -2,43 +2,64 @@ import { combineEpics } from 'redux-observable';
 import { Observable } from 'rxjs';
 import ServerDiscoverer from '../../utils/serverDiscoverer';
 
-const SERVICE_ANNOUNCED = 'SERVERS/SERVICE_ANNOUNCED';
+const SERVICE_ANNOUNCED = Symbol('SERVERS/SERVICE_ANNOUNCED');
 const SERVICE_RESOLVED = Symbol('SERVERS/SERVICE_RESOLVED');
-const SERVICE_REMOVED = 'SERVERS/SERVICE_REMOVED';
+const SERVICE_REMOVED = Symbol('SERVERS/SERVICE_REMOVED');
 
 const initialState = {
-
+  services: [],
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case SERVICE_ANNOUNCED:
+      return {
+        ...state,
+        services: [...state.services].splice(action.service.serviceIndex, 1, action.service),
+      };
     case SERVICE_RESOLVED:
+      return {
+        ...state,
+        services: [...state.services].splice(action.service.serviceIndex, 1, action.service),
+      };
     case SERVICE_REMOVED:
+      return {
+        ...state,
+        services: [...state.services].splice(action.service.serviceIndex, 1),
+      };
     default:
       return state;
   }
 }
 
-const serviceAnnounced = () => ({
+const serviceAnnounced = service => ({
   type: SERVICE_ANNOUNCED,
+  service: {
+    ...service,
+    status: 'ANNOUNCED',
+  },
 });
 
-const serviceResolved = () => ({
+const serviceResolved = service => ({
   type: SERVICE_RESOLVED,
+  service: {
+    ...service,
+    status: 'RESOLVED',
+  },
 });
 
-const serviceRemoved = () => ({
+const serviceRemoved = service => ({
   type: SERVICE_REMOVED,
+  service,
 });
 
 const serverDiscoveryEpic = () => {
   const serverDiscoverer = new ServerDiscoverer();
 
   return Observable.merge(
-    Observable.fromEvent(serverDiscoverer, 'SERVICE_ANNOUNCED').map((...args) => serviceAnnounced(args)),
-    Observable.fromEvent(serverDiscoverer, 'SERVICE_RESOLVED').map((...args) => serviceResolved(args)),
-    Observable.fromEvent(serverDiscoverer, 'SERVICE_REMOVED').map((...args) => serviceRemoved(args)),
+    Observable.fromEvent(serverDiscoverer, 'SERVICE_ANNOUNCED').map(service => serviceAnnounced(service)),
+    Observable.fromEvent(serverDiscoverer, 'SERVICE_RESOLVED').map(service => serviceResolved(service)),
+    Observable.fromEvent(serverDiscoverer, 'SERVICE_REMOVED').map(service => serviceRemoved(service)),
   );
 };
 
