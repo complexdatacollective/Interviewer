dc690c10f2e88aeb59e82e446f01bbc015396866
Rebecca Madsen <rmadsen@hailmail.net>
2018-02-23 13:16:07 -0500

    load protocol from url

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
   HashRouter as Router,
   Route,
@@ -8,6 +8,9 @@ import {
   Switch,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { actionCreators as protocolActions } from './ducks/modules/protocol';
 import {
   ProtocolScreen,
   SetupScreen,
@@ -19,26 +22,55 @@ function mapStateToProps(state) {
   };
 }
 
-let SetupRequiredRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={props => (
-      rest.isProtocolLoaded ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: '/setup' }} />
-      )
-    )}
-  />
-);
+function mapDispatchToProps(dispatch) {
+  return {
+    loadFactoryProtocol: bindActionCreators(protocolActions.loadFactoryProtocol, dispatch),
+  };
+}
 
-SetupRequiredRoute = connect(mapStateToProps)(SetupRequiredRoute);
+class SetupRequiredRoute extends Component {
+  componentWillMount() {
+    if (this.props.computedMatch.params && this.props.computedMatch.params.protocolId) {
+      this.props.loadFactoryProtocol(this.props.computedMatch.params.protocolId);
+    }
+  }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.computedMatch.params &&
+        nextProps.computedMatch.params !== this.props.computedMatch.params &&
+        nextProps.computedMatch.params.protocolId) {
+      this.props.loadFactoryProtocol(nextProps.computedMatch.params.protocolId);
+    }
+  }
+
+  render() {
+    const {
+      component: Component,
+      ...rest
+    } = this.props;
+
+    return (
+      <Route
+        {...rest}
+        render={props => (
+          rest.isProtocolLoaded ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to={{ pathname: '/setup' }} />
+          )
+        )}
+      />
+    );
+  }
+}
+
+SetupRequiredRoute = connect(mapStateToProps, mapDispatchToProps)(SetupRequiredRoute);
 
 export default () => (
   <Router>
     <Switch>
-      <SetupRequiredRoute path="/protocol" component={ProtocolScreen} />
+      <SetupRequiredRoute exact path="/protocol" component={ProtocolScreen} />
+      <SetupRequiredRoute path="/protocol/:protocolId" component={ProtocolScreen} />
       <Route path="/setup" component={SetupScreen} />
       <Redirect to={{ pathname: '/setup' }} />
     </Switch>
