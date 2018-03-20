6926e4873f7e3c514c0a6c22996e6799cb093d98
Joshua Melville <joshua.melville@northwestern.edu>
2018-03-20 11:45:52 +0000

    fix test

--- src/utils/__mocks__/CSSVariables.js
+++ src/utils/__mocks__/CSSVariables.js
@@ -1,16 +1,11 @@
-// import { get } from 'lodash';
+import { get } from 'lodash';
 
-// const mockCSSVariables = {
-//   '--ring': '#995522',
-//   '--background': '#227733',
-// };
-
-// const getCSSVariable = cssVariableName =>
-//   get(mockCSSVariables, cssVariableName);
-
-const getCSSVariable = (variableName) => {
-  const style = getComputedStyle(document.body);
-  return style.getPropertyValue(variableName).trim();
+const mockCSSVariables = {
+  '--ring': '#995522',
+  '--background': '#227733',
 };
+
+const getCSSVariable = cssVariableName =>
+  get(mockCSSVariables, cssVariableName);
 
 export default getCSSVariable;
