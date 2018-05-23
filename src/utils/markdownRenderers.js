ae31b8b170afc6bbb678c250008306f9e0e5aa11
Bryan Fox <git@bryfox.com>
2018-05-23 16:21:58 +0100

    Open content URLs in default browser
    Move render code to shared util
    
    Fixes #432

---
+++ src/utils/markdownRenderers.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import emoji from 'emoji-dictionary';
+
+const emojiTextRenderer = text => text.replace(/:\w+:/gi, name => emoji.getUnicode(name));
+
+const externalLinkRenderer = ({ href, children }) => (
+  <a href={href} target="_blank">
+    {children}
+  </a>
+);
+
+externalLinkRenderer.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType(PropTypes.element, PropTypes.array).isRequired,
+};
+
+const defaultMarkdownRenderers = {
+  text: emojiTextRenderer,
+  link: externalLinkRenderer,
+};
+
+export default defaultMarkdownRenderers;
+
+export {
+  emojiTextRenderer,
+  externalLinkRenderer,
+};
