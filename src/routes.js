Kwill147/Network-Canvas

Begin refactoring for pairing setup
Discovery & Pairing is now hidden behind the action button on Setup.

- Revert ServerList to pre-prototype state (mostly)
- Move ServerList to containers
- Add basic UI components & styles for pairing

src/routes.js
@@ -10,8 +10,9 @@ import { connect } from 'react-redux';
 import {
   LoadParamsRoute,
   ProtocolScreen,
-  SetupScreen,
 } from './containers';
+
+import { ProtocolImport, SetupScreen } from './containers/Setup';
 
 function mapStateToProps(state) {
   return {
@@ -46,6 +47,7 @@ export default () => (
     <SetupRequiredRoute exact path="/protocol" component={ProtocolScreen} />
     <LoadParamsRoute path="/protocol/:protocolType/:protocolId/:stageIndex" component={ProtocolScreen} />
     <LoadParamsRoute path="/protocol/:protocolType/:protocolId" component={ProtocolScreen} />
+    <LoadParamsRoute path="/protocol-import" component={ProtocolImport} />
     <LoadParamsRoute path="/reset" shouldReset component={Redirect} to={{ pathname: '/setup' }} />
     <Route path="/setup" component={SetupScreen} />
     <Redirect to={{ pathname: '/setup' }} />

complexdatacollective/Interviewer

src/routes.js
@@ -10,8 +10,9 @@ import { connect } from 'react-redux';
 import {
   LoadParamsRoute,
   ProtocolScreen,
-  SetupScreen,
 } from './containers';
+
+import { ProtocolImport, SetupScreen } from './containers/Setup';
 
 function mapStateToProps(state) {
   return {
@@ -51,6 +52,7 @@ export default () => (
     <LoadParamsRoute path="/session/:sessionId/:protocolType/:protocolId/:stageIndex" component={ProtocolScreen} />
     <LoadParamsRoute path="/session/:sessionId/:protocolType/:protocolId" component={ProtocolScreen} />
     <LoadParamsRoute path="/reset" shouldReset component={Redirect} to={{ pathname: '/setup' }} />
+    <Route path="/protocol-import" component={ProtocolImport} />
     <Route path="/setup" component={SetupScreen} />
     <Redirect to={{ pathname: '/setup' }} />
   </Switch>