3866d3a936eb5774b1e980bbff632e9e35cc4220
Steve Mckellar <steve@wwqrd.uk>
2018-06-18 16:13:51 +0100

    Add fifo iteratee to sortOrder

--- src/utils/sortOrder.js
+++ src/utils/sortOrder.js
@@ -1,9 +1,13 @@
 import { orderBy } from 'lodash';
 
+const fifo = (node, index) => index;
+
 // TODO: Use variable registry to respect variable type?
 const sortOrder = (sortConfiguration, variableRegistry) => {
-  const iteratees = sortConfiguration.map(item => item.variable);
-  const orders = sortConfiguration.map(item => item.direction);
+  const iteratees = sortConfiguration.map(rule => rule.property)
+    .map(property => (property === '*' ? fifo : property));
+
+  const orders = sortConfiguration.map(rule => rule.direction);
 
   return items =>
     orderBy(items, iteratees, orders);
