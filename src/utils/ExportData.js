d40b96db50a06725d9ff5962db0a44eb7cf31b58
Bryan Fox <git@bryfox.com>
2018-07-30 09:03:00 -0400

    Fix graphml export format
    Translate uid to id attribute in xml

--- src/utils/ExportData.js
+++ src/utils/ExportData.js
@@ -1,6 +1,7 @@
 import { findKey, forInRight, isNil, join } from 'lodash';
 
 import saveFile from './SaveFile';
+import { NodePK } from '../ducks/modules/network';
 
 const setUpXml = () => {
   const xmlDoc = '<?xml version="1.0" encoding="UTF-8"?>\n' +
@@ -146,8 +147,8 @@ const addElements = (graph, uri, dataList, type, excludeList, variableRegistry, 
   extra: false) => {
   dataList.forEach((dataElement, index) => {
     const domElement = document.createElementNS(uri, type);
-    if (dataElement.id) {
-      domElement.setAttribute('id', dataElement.id);
+    if (dataElement[NodePK]) {
+      domElement.setAttribute('id', dataElement[NodePK]);
     } else {
       domElement.setAttribute('id', index);
     }
@@ -199,8 +200,8 @@ const createGraphML = (networkData, variableRegistry, openErrorDialog) => {
   });
 
   // generate keys for attributes
-  let missingVariables = generateKeys(graph, graphML, networkData.nodes, 'node', ['id'], variableRegistry, layoutVariable);
-  missingVariables = missingVariables.concat(generateKeys(graph, graphML, networkData.edges, 'edge', ['from', 'to', 'id'], variableRegistry));
+  let missingVariables = generateKeys(graph, graphML, networkData.nodes, 'node', [NodePK], variableRegistry, layoutVariable);
+  missingVariables = missingVariables.concat(generateKeys(graph, graphML, networkData.edges, 'edge', ['from', 'to'], variableRegistry));
   if (missingVariables.length > 0) {
     // hard fail if checking the registry fails
     // remove this to fall back to using "text" for unknowns
@@ -209,8 +210,8 @@ const createGraphML = (networkData, variableRegistry, openErrorDialog) => {
   }
 
   // add nodes and edges to graph
-  addElements(graph, graphML.namespaceURI, networkData.nodes, 'node', ['id'], variableRegistry, layoutVariable);
-  addElements(graph, graphML.namespaceURI, networkData.edges, 'edge', ['from', 'to', 'id'], variableRegistry, null, true);
+  addElements(graph, graphML.namespaceURI, networkData.nodes, 'node', [NodePK], variableRegistry, layoutVariable);
+  addElements(graph, graphML.namespaceURI, networkData.edges, 'edge', ['from', 'to'], variableRegistry, null, true);
 
   return saveFile(xmlToString(xml), openErrorDialog, 'graphml', ['graphml'], 'networkcanvas.graphml', 'text/xml',
     { message: 'Your network canvas graphml file.', subject: 'network canvas export' });
