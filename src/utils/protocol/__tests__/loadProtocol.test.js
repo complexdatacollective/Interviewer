18a227c1a76acc71eaf7c83cf522b0e86caa4198
Bryan Fox <git@bryfox.com>
2018-10-03 20:00:47 -0400

    Assert that one stage is defined on load

--- src/utils/protocol/__tests__/loadProtocol.test.js
+++ src/utils/protocol/__tests__/loadProtocol.test.js
@@ -11,18 +11,30 @@ jest.mock('../protocolPath');
 
 describe('loadProtocol', () => {
   describe('Electron', () => {
+    let mockProtocol;
     beforeAll(() => {
+      mockProtocol = { foo: 'bar', stages: [{}] };
       getEnvironment.mockReturnValue(environments.ELECTRON);
-      readFile.mockReturnValue(Promise.resolve('{ "foo": "bar" }'));
     });
 
-    it('returns the parsed protocol object', () => {
-      expect(loadProtocol('bazz.protocol')).resolves.toEqual({
-        foo: 'bar',
-      }); // TODO: This should return in order to work...
+    beforeEach(() => {
+      readFile.mockReturnValue(Promise.resolve(JSON.stringify(mockProtocol)));
+    });
 
+    it('returns the parsed protocol object', async () => {
+      await expect(loadProtocol('bazz.protocol')).resolves.toEqual(mockProtocol);
       expect(protocolPath.mock.calls[0]).toEqual(['bazz.protocol', 'protocol.json']);
       expect(readFile.mock.calls[0]).toEqual(['tmp/mock/path/protocols/bazz.protocol/protocol.json']);
+    });
+
+    describe('when protocol has no stages', () => {
+      beforeAll(() => {
+        mockProtocol = { foo: 'bar' };
+      });
+
+      it('rejects loading', async () => {
+        await expect(loadProtocol('bazz.protocol')).rejects.toMatchObject({ message: 'Invalid protocol' });
+      });
     });
   });
 });
--- src/utils/protocol/loadProtocol.js
+++ src/utils/protocol/loadProtocol.js
@@ -6,11 +6,23 @@ import friendlyErrorMessage from '../../utils/friendlyErrorMessage';
 
 const openError = friendlyErrorMessage("We couldn't open that Network Canvas protocol. Check the format, and try again.");
 
+// Basic validation on protocol format;
+// any error will halt loading and display a message to the user.
+const verifyProtocol = (protocol) => {
+  if (!protocol.stages || !protocol.stages.length) {
+    const noStagesError = new Error('Invalid protocol');
+    noStagesError.friendlyMessage = 'Invalid protocol: no stages defined';
+    throw noStagesError;
+  }
+  return protocol;
+};
+
 const loadProtocol = (environment) => {
   if (environment !== environments.WEB) {
     return protocolName =>
       readFile(protocolPath(protocolName, 'protocol.json'))
         .then(data => JSON.parse(data))
+        .then(verifyProtocol)
         .catch(openError);
   }
 
