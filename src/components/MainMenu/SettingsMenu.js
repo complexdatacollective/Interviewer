acc53c362b5c18a5573b287cd4caff8ac1fd6b14
Steve Mckellar <steve@wwqrd.uk>
2018-10-12 10:17:42 +0100

    Separate out MenuPanel

--- src/components/MainMenu/SettingsMenu.js
+++ src/components/MainMenu/SettingsMenu.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
-import cx from 'classnames';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from '../../ui/components';
+import MenuPanel from './MenuPanel';
 
-// eslint-disable-next-line
-class SettingsMenu extends Component {
-  render() {
-    const { active, onClickInactive } = this.props;
-    const handleClickInactive = !active ? onClickInactive : null;
-
-    return (
-      <div
-        className={cx(
-          'menu-panel',
-          'menu-panel__settings',
-          { 'menu-panel--active': active },
-        )}
-        onClick={handleClickInactive}
-      >
-        <Icon name="settings" />
-        <div className="settings-menu">
-          <div className="settings-menu__header">
-            <h1>Settings</h1>
-          </div>
-          <div className="settings-menu__form">
-            <p>Some settings and things will go here.</p>
-          </div>
-        </div>
+const SettingsMenu = ({ active, onClickInactive }) => (
+  <MenuPanel
+    active={active}
+    panel="settings"
+    onClickInactive={onClickInactive}
+  >
+    <Icon name="settings" />
+    <div className="settings-menu">
+      <div className="settings-menu__header">
+        <h1>Settings</h1>
       </div>
-    );
-  }
-}
+      <div className="settings-menu__form">
+        <p>Some settings and things will go here.</p>
+      </div>
+    </div>
+  </MenuPanel>
+);
 
 SettingsMenu.propTypes = {
   active: PropTypes.bool,
--- src/components/MainMenu/StagesMenu.js
+++ src/components/MainMenu/StagesMenu.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 import Timeline from '../../components/MainMenu/Timeline';
 import StatsPanel from './StatsPanel';
 import { Icon } from '../../ui/components';
+import MenuPanel from './MenuPanel';
 
-const StagesMenu = ({ active, onClickInactive, currentStages }) => {
-  const handleClickInactive = !active ? onClickInactive : null;
-
-  return (
-    <div
-      className={cx(
-        'menu-panel',
-        'menu-panel__stages',
-        { 'menu-panel--active': active },
-      )}
-      onClick={handleClickInactive}
-    >
-      <Icon name="menu-default-interface" />
-      <div className="stages-menu" >
-        <div className="stages-menu__timeline">
-          <div className="stages-timeline__header">
-            <h1>Interview Stages</h1>
-            <input className="stages-input" type="text" placeholder="Filter stages..." />
-          </div>
-          <Timeline items={currentStages} />
+const StagesMenu = ({ active, onClickInactive, currentStages }) => (
+  <MenuPanel
+    active={active}
+    panel="stages"
+    onClickInactive={onClickInactive}
+  >
+    <Icon name="menu-default-interface" />
+    <div className="stages-menu" >
+      <div className="stages-menu__timeline">
+        <div className="stages-timeline__header">
+          <h1>Interview Stages</h1>
+          <input className="stages-input" type="text" placeholder="Filter stages..." />
         </div>
-        <StatsPanel />
+        <Timeline items={currentStages} />
       </div>
+      <StatsPanel />
     </div>
-  );
-};
+  </MenuPanel>
+);
 
 StagesMenu.propTypes = {
   currentStages: PropTypes.array.isRequired,
