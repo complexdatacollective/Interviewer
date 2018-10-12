07d05411bfdb4be5620e0f9bbc60f1706bf57723
Steve Mckellar <steve@wwqrd.uk>
2018-10-12 16:36:49 +0100

    Match StatsPanel syntax to other MainMenu containers

--- src/containers/MainMenu/StatsPanel.js
+++ src/containers/MainMenu/StatsPanel.js
@@ -1,15 +1,27 @@
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import { compose, withHandlers } from 'recompose';
 import StatsPanel from '../../components/MainMenu/StatsPanel';
 import { actionCreators as uiActions } from '../../ducks/modules/ui';
 import { actionCreators as sessionActions } from '../../ducks/modules/session';
 
+const statsPanelHandlers = withHandlers({
+  onFinishInterview: ({ closeMenu, endSession }) =>
+    () => {
+      closeMenu();
+      endSession();
+    },
+});
+
 const mapDispatchToProps = dispatch => ({
-  onFinishInterview: () => {
-    dispatch(uiActions.update({ isMenuOpen: false }));
+  closeMenu: () => dispatch(uiActions.update({ isMenuOpen: false })),
+  endSession: () => {
     dispatch(sessionActions.endSession());
     dispatch(push('/'));
   },
 });
 
-export default connect(null, mapDispatchToProps)(StatsPanel);
+export default compose(
+  connect(null, mapDispatchToProps),
+  statsPanelHandlers,
+)(StatsPanel);
