a79d6277bac0f09eb958375ec2d384d53487d701
Steve Mckellar <steve@wwqrd.uk>
2018-12-20 14:31:11 +0000

    Improve item sizing using float

--- src/containers/CategoricalList.js
+++ src/containers/CategoricalList.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 import { compose, bindActionCreators } from 'redux';
+import { debounce } from 'lodash';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Flipper } from 'react-flip-toolkit';
 import cx from 'classnames';
-
 import { makeNetworkNodesForType, makeGetVariableOptions, makeGetPromptVariable, makeGetNodeDisplayVariable } from '../selectors/interface';
 import { actionCreators as sessionsActions } from '../ducks/modules/sessions';
 import { CategoricalItem } from '../components/';
 import { MonitorDragSource } from '../behaviours/DragAndDrop';
 import { getCSSVariableAsString } from '../ui/utils/CSSVariables';
 import { getNodeAttributes, nodeAttributesProperty, nodePrimaryKeyProperty } from '../ducks/modules/network';
+import getAbsoluteBoundingRect from '../utils/getAbsoluteBoundingRect';
+
+const getIsPortrait = bounds =>
+  bounds.width / bounds.height < 1;
+
+const getExpandedSize = bounds => (
+  bounds.height > (bounds.width * 0.67) ?
+    Math.floor(bounds.width * 0.67) :
+    bounds.height
+);
+
+const getExpandedSizeVariables = bounds => ([
+  ['--categorical-list-expanded-bin-size', `${getExpandedSize(bounds)}px`],
+]);
+
+const getItemSizeVariables = (bounds, itemCount) => {
+  const isPortrait = getIsPortrait(bounds);
+
+  const shortCount = [4, 5, 6, 7, 8].includes(itemCount) ? 2 : 1;
+  const longCount = shortCount > 1 ? Math.ceil(itemCount / 2) : itemCount;
+
+  const expandedSize = getExpandedSize(bounds);
+
+  const width = bounds.width - expandedSize;
+
+  const longSide = isPortrait ? bounds.height : width;
+  const shortSide = isPortrait ? width : bounds.height;
+
+  const x = Math.floor(longSide / longCount);
+  const y = Math.floor(shortSide / shortCount);
+  const z = x < y ? x : y;
+
+  return [
+    ['--categorical-item-size', `${z}px`],
+    ['--categorical-list-rows', `${isPortrait ? longCount : shortCount}`],
+    ['--categorical-list-columns', `${isPortrait ? shortCount : longCount}`],
+  ];
+}
 
 /**
   * CategoricalList: Renders a list of categorical bin items
@@ -18,15 +56,28 @@ import { getNodeAttributes, nodeAttributesProperty, nodePrimaryKeyProperty } fro
 class CategoricalList extends Component {
   constructor(props) {
     super(props);
-    this.categoricalComponent = React.createRef();
+    this.categoricalListElement = React.createRef();
     this.state = {
       expandedBinValue: '',
     };
   }
 
-  getAvailableHeight() {
-    return this.categoricalComponent.current ? this.categoricalComponent.current.offsetHeight : 0;
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+    this.onResize();
   }
+
+  componentDidUpdate() {
+    this.onResize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  onResize = debounce(() => {
+    this.updateStyles();
+  }, 1000 / 60);
 
   getDetails = (nodes) => {
     if (nodes.length === 0) {
@@ -43,6 +94,37 @@ class CategoricalList extends Component {
 
     return '';
   };
+
+  getSizeVariables = () => {
+    if (!this.categoricalListElement.current) {
+      return [];
+    }
+
+    const categoricalListElement = this.categoricalListElement.current;
+
+    const bounds = getAbsoluteBoundingRect(categoricalListElement);
+
+    const expandedSize = getExpandedSizeVariables(bounds);
+    const itemSize = getItemSizeVariables(bounds, this.props.bins.length);
+
+    return [...expandedSize, ...itemSize];
+  };
+
+  updateStyles = () => {
+    if (!this.categoricalListElement.current) {
+      return;
+    }
+
+    const categoricalListElement = this.categoricalListElement.current;
+
+    const styles = this.getSizeVariables();
+
+    categoricalListElement.style = styles
+      .map(([property, value]) => `${property}: ${value};`)
+      .join(' ');
+
+    categoricalListElement.classList.add('categorical-list__items--sized');
+  }
 
   expandBin = (e, binValue) => {
     if (e) e.stopPropagation();
@@ -82,52 +164,62 @@ class CategoricalList extends Component {
     };
 
     return (
-      <CategoricalItem
-        id={`CATBIN_ITEM_${this.props.stage.id}_${this.props.prompt.id}_${index}`}
-        key={index}
-        label={bin.label}
-        accentColor={getCatColor(index)}
-        onDrop={item => onDrop(item)}
-        onClick={e => this.expandBin(e, bin.value)}
-        details={binDetails}
-        isExpanded={this.state.expandedBinValue === bin.value}
-        nodes={bin.nodes}
-        sortOrder={this.props.prompt.binSortOrder}
-      />
+      <div className="categorical-list__item">
+        <CategoricalItem
+          id={`CATBIN_ITEM_${this.props.stage.id}_${this.props.prompt.id}_${bin.value}`}
+          key={bin.value}
+          label={bin.label}
+          accentColor={getCatColor(index)}
+          onDrop={item => onDrop(item)}
+          onClick={e => this.expandBin(e, bin.value)}
+          details={binDetails}
+          isExpanded={this.state.expandedBinValue === bin.value}
+          nodes={bin.nodes}
+          sortOrder={this.props.prompt.binSortOrder}
+        />
+      </div>
     );
   }
 
   render() {
-    const classNames = cx(
-      'categorical-list__content',
-      { 'categorical-list__content--expanded': this.state.expandedBinValue },
+    const listClasses = cx(
+      'categorical-list',
+      { 'categorical-list--expanded': this.state.expandedBinValue },
     );
-    const styleNames = { '--num-categorical-items': this.props.bins.length };
-    if (this.props.bins.length < 4) {
-      styleNames['--categorical-item-multiplier'] = 1.2;
-    }
+
+    const listItemsClasses = cx(
+      'categorical-list__items',
+      `categorical-list__items--${this.props.bins.length}`,
+    );
+
+    // Render before filter, because we need to preserve order for colors.
+    const categoricalBins = this.props.bins
+      .map(this.renderCategoricalBin)
+      .filter((bin, index) =>
+        this.props.bins[index].value !== this.state.expandedBinValue,
+      );
+
+    // Render before filter, because we need to preserve order for colors.
+    const expandedBin = this.props.bins
+      .map(this.renderCategoricalBin)
+      .filter((bin, index) =>
+        this.props.bins[index].value === this.state.expandedBinValue,
+      );
 
     return (
-      <Flipper flipKey={this.state.expandedBinValue} className="categorical-list">
+      <Flipper
+        flipKey={this.state.expandedBinValue}
+        className={listClasses}
+      >
         <div
-          className="categorical-list__inner"
-          onClick={e => this.expandBin(e, '', '')}
-          ref={this.categoricalComponent}
-          style={{ '--categorical-available-height': `${Math.floor(this.getAvailableHeight())}px` }}
+          className={listItemsClasses}
+          ref={this.categoricalListElement}
         >
           <div className="categorical-list__expanded-bin">
-            {this.props.bins.map(this.renderCategoricalBin).filter((bin, index) =>
-              this.props.bins[index].value === this.state.expandedBinValue,
-            )}
+            {expandedBin}
           </div>
-          <div
-            className={classNames}
-            style={styleNames}
-          >
-            {this.props.bins.map(this.renderCategoricalBin).filter((bin, index) =>
-              this.props.bins[index].value !== this.state.expandedBinValue,
-            )}
-          </div>
+          {categoricalBins}
+
         </div>
       </Flipper>
     );
