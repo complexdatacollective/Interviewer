591ed47df98efab5e314884b0da47288d978d1cb
Joshua Melville <joshua.melville@northwestern.edu>
2019-03-27 11:49:02 +0000

    remove protocol validation code in preparation for submodule

--- src/utils/protocol/validation/index.js
+++
@@ -1,9 +0,0 @@
-const validateLogic = require('./validateLogic');
-const validateSchema = require('./validateSchema');
-const logErrors = require('./logErrors');
-
-module.exports = {
-  logErrors,
-  validateLogic,
-  validateSchema,
-};
