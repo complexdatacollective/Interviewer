9cf3799098dfd314100bc9932ca1238ccb689f45
Joshua Melville <joshua.melville@northwestern.edu>
2019-04-08 13:10:40 +0100

    update timeline stage icons

--- src/images/timeline/index.js
+++ src/images/timeline/index.js
@@ -1,5 +1,3 @@
-import Default from './stage--Default.png';
-import AlterForm from './stage--AlterForm.png';
 import CategoricalBin from './stage--CategoricalBin.png';
 import NameGenerator from './stage--NameGenerator.png';
 import NameGeneratorList from './stage--NameGeneratorList.png';
@@ -8,11 +6,12 @@ import OrdinalBin from './stage--OrdinalBin.png';
 import Sociogram from './stage--Sociogram.png';
 import Narrative from './stage--Narrative.png';
 import Information from './stage--Information.png';
-
+import AlterForm from './stage--AlterForm.png';
+import AlterEdgeForm from './stage--AlterEdgeForm.png';
+import EgoForm from './stage--EgoForm.png';
+import Default from './stage--Default.png';
 
 export default {
-  Default,
-  AlterForm,
   CategoricalBin,
   NameGenerator,
   NameGeneratorList,
@@ -21,4 +20,8 @@ export default {
   Information,
   OrdinalBin,
   Sociogram,
+  AlterForm,
+  AlterEdgeForm,
+  EgoForm,
+  Default,
 };
