92c001520535ba46a5aae5c6c3c919fa462784d3
Joshua Melville <joshua.melville@northwestern.edu>
2019-04-26 11:48:28 +0100

    fix errors epic

--- src/ducks/modules/errors.js
+++ src/ducks/modules/errors.js
@@ -2,22 +2,19 @@
 
 import { combineEpics } from 'redux-observable';
 // import { Observable } from 'rxjs';
-import { filter, mapTo } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 import { actionCreators as dialogActions } from './dialogs';
 import { actionTypes as importProtocolActionTypes } from './importProtocol';
 import { actionTypes as serverActionTypes } from './pairedServer';
-import { actionTypes as sessionsActionTypes } from './sessions';
 
 const errorActions = [
   importProtocolActionTypes.IMPORT_PROTOCOL_FAILED,
   serverActionTypes.SERVER_PAIRING_FAILED,
-  sessionsActionTypes.EXPORT_SESSION_FAILED,
 ];
-
 
 const errorsEpic = action$ => action$.pipe(
   filter(action => errorActions.includes(action.type)),
-  mapTo(action => dialogActions.openDialog({ type: 'Error', error: action.error })),
+  map(action => dialogActions.openDialog({ type: 'Error', error: action.error })),
 );
 
 const epics = combineEpics(
