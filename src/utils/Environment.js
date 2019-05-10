d2528ae6469d77a5c55bf543134167e926d68e93
Steve Mckellar <steve@wwqrd.uk>
2019-05-10 15:30:24 +0100

    Fix environment checks

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -2,11 +2,12 @@ import environments from './environments';
 
 export const isElectron = () => !!window.require;
 
-export const isMacOS = () => isElectron && window.require && window.require('os').platform() === 'darwin';
 
-export const isWindows = () => isElectron && window.require && window.require('os').platform() === 'win32';
+export const isMacOS = () => isElectron() && window.require('os').platform() === 'darwin';
 
-export const isLinux = () => isElectron && window.require && window.require('os').platform() === 'linux';
+export const isWindows = () => isElectron() && window.require('os').platform() === 'win32';
+
+export const isLinux = () => isElectron() && window.require('os').platform() === 'linux';
 
 export const isCordova = () => !!window.cordova;
 
