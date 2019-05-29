dd898511a7780fdeba10abb9d79ccafe550e71e1
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-29 13:35:56 +0100

    update protocol validation and development protocol version

--- src/components/MainMenu/SettingsMenu.js
+++ src/components/MainMenu/SettingsMenu.js
@@ -122,7 +122,7 @@ class SettingsMenu extends PureComponent {
                 <section>
                   <Button
                     color="mustard"
-                    onClick={() => importProtocolFromURI('https://github.com/codaco/development-protocol/releases/download/20190520140749-4afad6a/development-protocol.netcanvas')}
+                    onClick={() => importProtocolFromURI('https://github.com/codaco/development-protocol/releases/download/20190529123247-7c1e58a/development-protocol.netcanvas')}
                   >
                     Import development protocol
                   </Button>
