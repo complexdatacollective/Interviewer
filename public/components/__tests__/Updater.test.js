e5858a7f42459d488fe4ab9512f006554206048c
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-31 16:32:27 +0100

    change updater to Updater

---
+++ public/components/__tests__/Updater.test.js
@@ -0,0 +1,37 @@
+/* eslint-env jest */
+const Updater = require('../Updater');
+const { dialog } = require('electron');
+
+jest.mock('electron');
+jest.mock('electron-updater');
+jest.mock('electron-log');
+
+let updater;
+
+describe('updater', () => {
+  beforeEach(() => {
+    dialog.showMessageBox.mockClear();
+    updater = new Updater();
+  });
+
+  it('shows a message when update available', () => {
+    updater.simulate('update-available', {});
+    expect(dialog.showMessageBox).toHaveBeenCalled();
+  });
+
+  it('shows a message when download ready', () => {
+    updater.simulate('update-downloaded', {});
+    expect(dialog.showMessageBox).toHaveBeenCalled();
+  });
+
+  it('shows a message when no update available', () => {
+    updater.checkForUpdates();
+    updater.simulate('update-not-available', {});
+    expect(dialog.showMessageBox).toHaveBeenCalled();
+  });
+
+  it('shows errors to the user', () => {
+    updater.simulate('error', new Error());
+    expect(dialog.showMessageBox).toHaveBeenCalled();
+  });
+});
