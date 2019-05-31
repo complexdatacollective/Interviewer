3d0624777d58896422a1b3eb89a8b89e57c91658
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-31 11:11:26 +0100

    check for update on startup, but don't notify if none found

--- public/components/updater.js
+++ public/components/updater.js
@@ -1,51 +1,72 @@
+/* eslint-disable class-methods-use-this */
+
 const { autoUpdater } = require('electron-updater');
 const { dialog } = require('electron');
 const log = require('./log');
+const EventEmitter = require('events').EventEmitter;
 
-const releasesUrl = 'https://github.com/codaco/Network-Canvas/releases';
+class Updater {
+  constructor() {
+    this.releasesUrl = 'https://github.com/codaco/Network-Canvas/releases';
+    this.events = new EventEmitter();
+    autoUpdater.autoDownload = false;
+    autoUpdater.on('error', this.onError);
+    autoUpdater.on('update-available', this.onUpdateAvailable.bind(this));
+    autoUpdater.on('update-downloaded', this.onUpdateDownloaded);
+    autoUpdater.on('update-not-available', this.onUpdateNotAvailable.bind(this));
+  }
 
-const onUpdateAvailable = (updateInfo) => {
-  dialog.showMessageBox({
-    type: 'question',
-    title: 'Update Available',
-    message: 'Do you want update now?',
-    detail: `Version ${updateInfo.releaseName} is available.\n\nRelease notes are available:\n${releasesUrl}`,
-    buttons: ['Update and Restart', 'Cancel'],
-  },
-  (buttonIndex) => {
-    if (buttonIndex === 0) {
-      autoUpdater.downloadUpdate();
+  onUpdateAvailable(updateInfo) {
+    dialog.showMessageBox({
+      type: 'question',
+      title: 'Update Available',
+      message: 'Do you want update now?',
+      detail: `Version ${updateInfo.releaseName} is available.\n\nRelease notes are available at:\n${this.releasesUrl}\n\nClick 'Download and Restart' to fetch this update and install it. Ensure you have exported or backed up any important data before continuing.`,
+      buttons: ['Download and Restart', 'Cancel'],
+    },
+    (buttonIndex) => {
+      if (buttonIndex === 0) {
+        autoUpdater.downloadUpdate();
+      }
+    });
+  }
+
+  onUpdateNotAvailable() {
+    if (this.notifyOnNoUpdates) {
+      dialog.showMessageBox({
+        title: 'No Updates Available',
+        message: 'Network Canvas is up-to-date.',
+      });
+    } else {
+      log.info('No updates available (did not notify user).');
     }
-  });
-};
+  }
 
-const onUpdateNotAvailable = () => {
-  dialog.showMessageBox({
-    title: 'No Updates Available',
-    message: 'Network Canvas is up-to-date.',
-  });
-};
+  onUpdateDownloaded() {
+    dialog.showMessageBox({
+      title: 'Install Update',
+      message: 'Download Complete',
+      detail: 'Your update is ready to install. You must now restart the app and install the update.',
+      buttons: ['Restart'],
+    },
+    () => setImmediate(() => autoUpdater.quitAndInstall()));
+  }
 
-const onUpdateDownloaded = () => {
-  dialog.showMessageBox({
-    title: 'Install Update',
-    message: 'Download Complete',
-    detail: 'Your update is ready to install. Click this notification to restart the app and install the update.',
-    buttons: ['Restart'],
-  },
-  () => setImmediate(() => autoUpdater.quitAndInstall()));
-};
+  onError(error) {
+    const detail = error ? (error.stack || error).toString() : 'An unknown error occurred';
+    log.error(detail);
+    dialog.showErrorBox('Error', 'There was an error checking for updates. You may need to update this app manually.');
+  }
 
-const onError = (error) => {
-  const detail = error ? (error.stack || error).toString() : 'An unknown error occurred';
-  log.error(detail);
-  dialog.showErrorBox('Error', 'There was an error checking for updates. You may need to update this app manually.');
-};
+  on(...args) {
+    this.events.on(...args);
+  }
 
-autoUpdater.autoDownload = false;
-autoUpdater.on('error', onError);
-autoUpdater.on('update-available', onUpdateAvailable);
-autoUpdater.on('update-downloaded', onUpdateDownloaded);
-autoUpdater.on('update-not-available', onUpdateNotAvailable);
+  checkForUpdates(notifyOnNoUpdates = true) {
+    this.notifyOnNoUpdates = !!notifyOnNoUpdates;
+    autoUpdater.checkForUpdates();
+  }
+}
 
-module.exports = autoUpdater;
+module.exports = Updater;
+
