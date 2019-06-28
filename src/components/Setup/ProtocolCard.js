90e4606bf3fee4a0c43fab916264135413842cd7
Steve Mckellar <steve@wwqrd.uk>
2019-06-28 15:48:40 +0100

    Fix ProtocolCard test

--- src/components/Setup/ProtocolCard.js
+++ src/components/Setup/ProtocolCard.js
@@ -3,18 +3,26 @@ import PropTypes from 'prop-types';
 
 import { Icon } from '../../ui/components';
 
-const ProtocolCard = ({ protocol, selectProtocol, onDelete }) => (
-  <div className="protocol-card" onClick={() => selectProtocol(protocol)}>
-    <div className="protocol-card__delete" onClick={(e) => { e.preventDefault(); e.stopPropagation(); onDelete(); }}>
-      <Icon name="delete" />
+const ProtocolCard = ({ protocol, selectProtocol, onDelete }) => {
+  const handleDelete = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onDelete();
+  };
+
+  return (
+    <div className="protocol-card" onClick={() => selectProtocol(protocol)}>
+      <div className="protocol-card__delete" onClick={handleDelete}>
+        <Icon name="delete" />
+      </div>
+      <h2 className="protocol-card__name">{protocol.name}</h2>
+      <div className="protocol-card__icon-wrapper">
+        <Icon className="protocol-card__icon" name="protocol-card" />
+      </div>
+      { protocol.description ? (<p className="protocol-card__description">{protocol.description}</p>) : ''}
     </div>
-    <h2 className="protocol-card__name">{protocol.name}</h2>
-    <div className="protocol-card__icon-wrapper">
-      <Icon className="protocol-card__icon" name="protocol-card" />
-    </div>
-    { protocol.description ? (<p className="protocol-card__description">{protocol.description}</p>) : ''}
-  </div>
-);
+  );
+};
 
 ProtocolCard.defaultProps = {
   className: '',
--- src/components/Setup/__tests__/ProtocolCard.test.js
+++ src/components/Setup/__tests__/ProtocolCard.test.js
@@ -8,15 +8,24 @@ describe('<ProtocolCard>', () => {
   let component;
   let mockProtocol;
   let selectHandler;
+  let deleteHandler;
 
   beforeEach(() => {
     selectHandler = jest.fn();
+    deleteHandler = jest.fn();
     mockProtocol = { name: 'my-mock-protocol', description: '1.0.1' };
-    component = shallow(<ProtocolCard selectProtocol={selectHandler} protocol={mockProtocol} />);
+    component = shallow((
+      <ProtocolCard
+        selectProtocol={selectHandler}
+        onDelete={deleteHandler}
+        protocol={mockProtocol}
+      />
+    ));
   });
 
   it('renders an icon', () => {
-    expect(component.find('Icon')).toHaveLength(1);
+    // close button and card icon
+    expect(component.find('Icon')).toHaveLength(2);
   });
 
   it('renders name & description', () => {
@@ -27,5 +36,11 @@ describe('<ProtocolCard>', () => {
   it('downloads on click', () => {
     component.simulate('click');
     expect(selectHandler).toHaveBeenCalledTimes(1);
+  });
+
+  it('deletes on click delete', () => {
+    const mockEvent = { stopPropagation: () => {}, preventDefault: () => {} };
+    component.find('.protocol-card__delete').simulate('click', mockEvent);
+    expect(deleteHandler).toHaveBeenCalledTimes(1);
   });
 });
