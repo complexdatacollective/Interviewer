ceedb212128f56c11b3cce71ff8be9483699514c
Steve Mckellar <steve@wwqrd.uk>
2019-08-20 11:17:28 +0100

    decodeURIComponent for url imported protocols

--- src/ducks/modules/__tests__/importProtocol.test.js
+++ src/ducks/modules/__tests__/importProtocol.test.js
@@ -1,7 +1,14 @@
 /* eslint-env jest */
-import reducer, { initialState } from '../importProtocol';
+import reducer, { initialState, helpers } from '../importProtocol';
 
-describe('protocol module', () => {
+describe('importProtocol', () => {
+  describe('helpers', () => {
+    it('filenameFromURI should correctly extract protocol name', () => {
+      const exampleUrl = 'https://documentation.networkcanvas.com/protocols/Public%20Health%20Protocol.netcanvas?foo=bar#bazz';
+      expect(helpers.filenameFromURI(exampleUrl)).toEqual('Public Health Protocol.netcanvas');
+    });
+  });
+
   describe('reducer', () => {
     it('should return the initial state', () => {
       expect(
