d8b3a49fe58f5a3e44219bb5740f05cdf0c095c7
Joshua Melville <joshua.melville@northwestern.edu>
2019-10-04 18:55:39 +0200

    case insensitivity

--- src/selectors/__tests__/network.test.js
+++ src/selectors/__tests__/network.test.js
@@ -16,6 +16,20 @@ const mockNodeCodebookDefinition = {
   },
 };
 
+const mockNodeCodebookDefinitionWithCaps = {
+  name: 'person',
+  variables: {
+    '80a0f680-3bc5-4bbe-af54-e05b387e82e6': {
+      type: 'text',
+      component: 'Text',
+      validation: {
+        required: true,
+      },
+      name: 'NaMe',
+    },
+  },
+};
+
 // Mock a typical node in an interview, with UID attribute name
 const mockNode1 = {
   '80a0f680-3bc5-4bbe-af54-e05b387e82e6': 'Node Label',
@@ -32,24 +46,50 @@ const mockNode3 = {
   name01: 'Node Label',
 };
 
+// Mock a typical node from an external data source where a name variable
+const mockNode4 = {
+  NamE: 'Node Label',
+};
+
+// Two valid name variables.
+const mockNode5 = {
+  NamE: 'First',
+  nAMe: 'Second',
+};
+
 
 describe('network selector', () => {
   describe('Node label logic', () => {
+    // Handles codebook lookup
     it('uses the codebook to find a node attribute called name and returns it', () => {
       const label = Network.labelLogic(mockNodeCodebookDefinition, mockNode1);
       expect(label).toEqual('Node Label');
     });
 
-    // handles external data
+    it('handles irregularly capitalised codebook variable name', () => {
+      const label = Network.labelLogic(mockNodeCodebookDefinitionWithCaps, mockNode1);
+      expect(label).toEqual('Node Label');
+    });
+
+    // Handles external data
     it('uses the a variable called \'name\' on the node itself', () => {
       const label = Network.labelLogic(mockNodeCodebookDefinition, mockNode2);
       expect(label).toEqual('Node Label');
     });
 
-    // fallback
-    it('returns warning message when no suitable label is available', () => {
+    it('handles external data with name in irregular case', () => {
+      const label = Network.labelLogic(mockNodeCodebookDefinition, mockNode4);
+      expect(label).toEqual('Node Label');
+    });
+
+    it('returns fallback message when no suitable label is available', () => {
       const label = Network.labelLogic(mockNodeCodebookDefinition, mockNode3);
       expect(label).toEqual('No \'name\' variable!');
+    });
+
+    it('correctly handles multiple possible node attributes by using the first', () => {
+      const label = Network.labelLogic(mockNodeCodebookDefinition, mockNode5);
+      expect(label).toEqual('First');
     });
   });
 });
--- src/selectors/network.js
+++ src/selectors/network.js
@@ -1,4 +1,4 @@
-import { findKey, get } from 'lodash';
+import { findKey, find } from 'lodash';
 import { getActiveSession, getCaseId } from './session';
 import { createDeepEqualSelector } from './utils';
 import { getProtocolCodebook } from './protocol';
@@ -72,14 +72,25 @@ export const labelLogic = (codebookForNodeType, nodeAttributes) => {
   // In the codebook for the stage's subject, look for a variable with a name
   // property of "name", and try to retrieve this value by key in the node's
   // attributes
-  const variableCalledName = codebookForNodeType && codebookForNodeType.variables && findKey(codebookForNodeType.variables, ['name', 'name']);
+  const variableCalledName =
+    codebookForNodeType &&
+    codebookForNodeType.variables &&
+    // Ignore case when looking for 'name'
+    findKey(codebookForNodeType.variables, variable => variable.name.toLowerCase() === 'name');
+
   if (variableCalledName && nodeAttributes[variableCalledName]) {
     return nodeAttributes[variableCalledName];
   }
 
-  // look for a property on the node with a key of ‘name’, and try to retrieve this
+  // Look for a property on the node with a key of ‘name’, and try to retrieve this
   // value as a key in the node's attributes.
-  const nodeVariableCalledName = get(nodeAttributes, 'name');
+  // const nodeVariableCalledName = get(nodeAttributes, 'name');
+
+  const nodeVariableCalledName = find(
+    nodeAttributes,
+    (_, key) => key.toLowerCase() === 'name',
+  );
+
   if (nodeVariableCalledName) {
     return nodeVariableCalledName;
   }
