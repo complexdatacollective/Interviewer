af4d037c74b228bc17ed01bb7fcae0172e0067e9
Joshua Melville <joshua.melville@northwestern.edu>
2019-10-10 13:39:28 +0100

    update networkQuery to correctly handle null categorical values

--- src/components/Setup/ProtocolCard.js
+++ src/components/Setup/ProtocolCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import cx from 'classnames';
 import { actionCreators as dialogActions } from '../../ducks/modules/dialogs';
