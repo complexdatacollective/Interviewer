09f04c4c6c95bc1fc0fffcc5c4d377a34af1bf05
Joshua Melville <joshua.melville@northwestern.edu>
2019-10-11 13:23:53 +0100

    update networkQuery reference

--- src/config.js
+++ src/config.js
@@ -5,4 +5,4 @@ export const APP_SCHEMA_VERSION = 2;
 
 export const APP_SUPPORTED_SCHEMA_VERSIONS = ['1.0.0', 1, 2];
 
-export const DEVELOPMENT_PROTOCOL_URL = 'https://github.com/codaco/development-protocol/releases/download/20191010151938-5fe12b9/Development.netcanvas';
+export const DEVELOPMENT_PROTOCOL_URL = 'hhttps://github.com/codaco/development-protocol/releases/download/20191011105845-fdad2bc/Development.netcanvas';
