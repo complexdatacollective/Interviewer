79a0e571fb619dd3d62a99c07262d4385240a8d4
Joshua Melville <joshua.melville@northwestern.edu>
2020-01-13 15:43:17 +0200

    Removed commented line

--- config/jest/automock.js
+++ config/jest/automock.js
@@ -5,6 +5,5 @@ jest.mock('fs');
 jest.mock('redux-logger');
 jest.mock('../../src/utils/Environment');
 jest.mock('../../src/utils/uuid');
-// jest.mock('../../src/ui/utils/CSSVariables');
 
 global.console.error = jest.fn();
