8dc81bff0cd6085af3625c1c175f415bf6feb954
Steve Mckellar <steve@wwqrd.uk>
2020-05-12 18:01:02 +0100

    WIP useSteps

--- src/containers/Interfaces/DyadCensus/useStageState.js
+++ src/containers/Interfaces/DyadCensus/useStageState.js
@@ -1,5 +1,63 @@
 import { useState } from 'react';
 
+const useSteps = (steps = [], onComplete) => {
+  // [1, 2]
+  const [state, setState] = useState({
+    progress: { step: null, prompt: null },
+    location: { step: 0, prompt: 0 },
+  });
+
+  const { location, progress } = state;
+
+  const next = () => {
+    const { step, prompt } = location;
+    const nextStep = step + 1 > steps[prompt] - 1 ? 0 : step + 1;
+    const nextPrompt = step + 1 > steps[prompt] - 1 ? prompt + 1 : prompt;
+
+    if (nextPrompt > steps.length) {
+      onComplete();
+      return;
+    }
+
+    const nextLocation = {
+      step: nextStep,
+      prompt: nextPrompt,
+    };
+
+    const nextProgress =
+      (location.nextStep > progress.step && location.nextPrompt >= progress.prompt) ?
+        { step: nextStep, prompt: nextPrompt } :
+        progress;
+
+    setState(s => ({
+      ...s,
+      location: nextLocation,
+      progress: nextProgress,
+    }));
+  };
+
+  const previous = () => {
+    const { step, prompt } = location;
+    const nextPrompt = step - 1 < 0 ? prompt - 1 : prompt;
+    const nextStep = step - 1 < 0 ? steps[nextPrompt] - 1 : step - 1;
+
+    if (nextPrompt < 0) {
+      onComplete();
+      return;
+    }
+
+    setState(s => ({
+      ...s,
+      location: {
+        step: nextStep,
+        prompt: nextPrompt,
+      },
+    }));
+  };
+
+  return [state, { next, previous }];
+};
+
 const useStageState = ({
   pairs,
   createEdge,
