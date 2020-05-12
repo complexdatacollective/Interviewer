7c0b3f03f12a3ac80ddaa69d22b222f33e3bf765
Steve Mckellar <steve@wwqrd.uk>
2020-05-12 14:32:20 +0100

    DyadCensus navigation

--- src/containers/Interfaces/DyadCensus.js
+++ src/containers/Interfaces/DyadCensus.js
@@ -2,13 +2,26 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import PropTypes from 'prop-types';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Button } from '@codaco/ui';
+import RadioGroup from '@codaco/ui/lib/components/Fields/RadioGroup';
 import withPrompt from '../../behaviours/withPrompt';
 import { actionCreators as sessionsActions } from '../../ducks/modules/sessions';
 import { entityPrimaryKeyProperty } from '../../ducks/modules/network';
-import {
-  makeNetworkNodesForPrompt as makeGetNodesForPrompt,
-} from '../../selectors/interface';
-import { PromptSwiper } from '../';
+import { makeNetworkNodesForPrompt as makeGetNodesForPrompt } from '../../selectors/interface';
+import { getNetworkEdges } from '../../selectors/network';
+import PromptSwiper from '../PromptSwiper';
+
+const variants = {
+  initial: { translateX: '100%', opacity: 0, position: 'absolute', top: 0, left: 0 },
+  enter: { translateX: '0%', opacity: 1 },
+  exit: { translateX: '-100%', opacity: 0 },
+};
+
+const options = [
+  { value: false, label: 'no' },
+  { value: true, label: 'yes' },
+];
 
 /**
   * Dyad Census Interface
@@ -22,29 +35,83 @@ const DyadCensus = ({
   stage: { prompts },
   pairs,
   nodes,
+  edges,
 }) => {
-  const [pairIndex, setPairIndex] = useState(0);
+  const [state, setState] = useState({
+    progress: null,
+    current: 0,
+    selected: null,
+  });
+
+  const next = () => {
+    // save edge state
+    setState(s => ({
+      ...s,
+      current: s.current + 1,
+      selected: null,
+      progress: s.progress > s.current + 1 ? s.progress : s.current + 1,
+    }));
+  };
+
+  const back = () => {
+    setState(s => ({
+      ...s,
+      current: s.current - 1,
+      selected: null,
+    }));
+  };
+
+  const select = (value) => {
+    setState(s => ({
+      ...s,
+      selected: value,
+    }));
+  };
 
   // TODO: Should this also receive an onComplete method?
   const beforeNext = useCallback((direction) => {
-    if (direction < 0) {
-      if (pairIndex > 0) {
-        setPairIndex(n => n - 1);
-        return;
-      }
-    }
-
-    if (direction > 1) {
-      console.log('yes or no only!');
+    if (direction < 0 && state.current > 0) {
+      back();
       return;
     }
 
-    onComplete();
-  }, [pairIndex, setPairIndex]);
+    console.log('yes or no only!');
+    next();
+    // could save and continue...s
+  }, [back, next]);
 
   useEffect(() => {
     registerBeforeNext(beforeNext);
   }, [beforeNext]);
+
+  const handleChange = useCallback((newValue) => {
+    select(newValue);
+  }, [select]);
+
+  const handleConfirm = useCallback(() => {
+    next();
+  }, [next]);
+
+  const currentPair = pairs[state.current];
+
+  const getCurrentValue = () => {
+    if (state.selected) { return state.selected; }
+
+    const edge = edges.find(({ from, to }) => (
+      from === currentPair[0] && to === currentPair[1] ||
+      to === currentPair[0] && from === currentPair[1]
+    ));
+
+    if (edge) {
+      return true;
+    }
+
+    if (!edge && state.progress > state.current) {
+      return false;
+    }
+
+    return null;
+  };
 
   return (
     <div className="interface">
@@ -58,8 +125,34 @@ const DyadCensus = ({
       </div>
       <div className="interface__main">
         <h3>PAIR</h3>
-        <p>{pairs.length}</p>
-        <p>{JSON.stringify(pairs[pairIndex])}</p>
+        <p>{state.current + 1}/{pairs.length}</p>
+        <div
+          style={{
+            position: 'relative',
+          }}
+        >
+          <AnimatePresence>
+            <motion.div
+              key={state.current}
+              variants={variants}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              <RadioGroup
+                input={{
+                  onChange: handleChange,
+                  value: getCurrentValue(),
+                }}
+                options={options}
+              />
+
+              <Button
+                onClick={handleConfirm}
+              >Confirm</Button>
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
@@ -74,7 +167,6 @@ DyadCensus.propTypes = {
   promptBackward: PropTypes.func.isRequired,
   promptForward: PropTypes.func.isRequired,
   stage: PropTypes.object.isRequired,
-  updateNode: PropTypes.func.isRequired,
 };
 
 const makeMapStateToProps = () => {
@@ -82,18 +174,38 @@ const makeMapStateToProps = () => {
 
   const mapStateToProps = (state, props) => {
     const nodes = getNodesForPrompt(state, props);
+    const edges = getNetworkEdges(state, props);
     const nodeIds = nodes.map(node => node[entityPrimaryKeyProperty]);
 
-    const pairs = nodeIds.flatMap(
-      id =>
-        nodeIds
-          .filter(alterId => alterId !== id)
-          .map(alterId => ([id, alterId])),
+    // const pairs = nodeIds.flatMap(
+    //   id =>
+    //     nodeIds
+    //       .filter(alterId => alterId !== id)
+    //       .map(alterId => ([id, alterId])),
+    // );
+    // mutally exclusive only
+    const pairs = nodeIds.reduce(
+      ({ result, pool }, id) => {
+        const nextPool = pool.filter(alterId => alterId !== id);
+
+        if (nextPool.length === 0) {
+          return result;
+        }
+
+        const newPairs = nextPool.map(alterId => ([id, alterId]));
+
+        return {
+          result: [...result, ...newPairs],
+          pool: nextPool,
+        };
+      },
+      { pool: nodeIds, result: [] },
     );
 
     return {
       pairs,
       nodes,
+      edges,
     };
   };
 
