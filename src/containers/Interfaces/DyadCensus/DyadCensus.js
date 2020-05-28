4cd958c1a8f9d7d071137c9aea8f2b8325d66ac3
Steve Mckellar <steve@wwqrd.uk>
2020-05-28 14:18:42 +0100

    Show invalid animation on dyad choice

--- src/containers/Interfaces/DyadCensus/DyadCensus.js
+++ src/containers/Interfaces/DyadCensus/DyadCensus.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { compose } from 'recompose';
 import PropTypes from 'prop-types';
 import { get } from 'lodash';
+import cx from 'classnames';
 import { AnimatePresence, motion } from 'framer-motion';
 import ReactMarkdown from 'react-markdown';
 import defaultMarkdownRenderers from '../../../utils/markdownRenderers';
@@ -56,6 +57,7 @@ const DyadCensus = ({
 }) => {
   const [isIntroduction, setIsIntroduction] = useState(true);
   const [isForwards, setForwards] = useState(true);
+  const [isValid, setIsValid] = useState(true);
 
   // Number of pairs times number of prompts e.g. `[3, 3, 3]`
   const steps = Array(stage.prompts.length).fill(get(pairs, 'length', 0));
@@ -75,6 +77,7 @@ const DyadCensus = ({
 
   const next = () => {
     setForwards(true);
+    setIsValid(true);
 
     if (isIntroduction) {
       // If we're on the introduction and also at the end of steps,
@@ -91,6 +94,7 @@ const DyadCensus = ({
 
     // check value has been set
     if (!canSkip && hasEdge === null) {
+      setIsValid(false);
       return;
     }
 
@@ -105,6 +109,7 @@ const DyadCensus = ({
 
   const back = () => {
     setForwards(false);
+    setIsValid(true);
 
     if (stepsState.isStart && !isIntroduction) {
       setIsIntroduction(true);
@@ -142,6 +147,11 @@ const DyadCensus = ({
       if (isTouched) { return; }
       setEdge(nextValue);
     };
+
+  const choiceClasses = cx(
+    'dyad-interface__choice',
+    { 'dyad-interface__choice--invalid': !isValid },
+  );
 
   return (
     <div className="interface dyad-interface">
@@ -206,7 +216,7 @@ const DyadCensus = ({
                   </AnimatePresence>
                 </div>
                 <motion.div
-                  className="dyad-interface__choice"
+                  className={choiceClasses}
                   variants={choiceVariants}
                   initial="hide"
                   animate={!isIntroduction ? 'show' : 'hide'}
