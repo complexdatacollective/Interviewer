f7bb6d6640d9274ed8584f329b369f4c14d3b92e
Joshua Melville <joshua.melville@northwestern.edu>
2020-05-28 15:14:52 +0100

    update babel and experimental image BB reporter

---
+++ integration-tests/scripts/image-reporter.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+/*
+ * To enable this image reporter, add it to your `jest.config.js` "reporters" definition:
+    "reporters": [ "default", "<rootDir>/image-reporter.js" ]
+ */
+
+const chalk = require('chalk');
+const fs = require('fs');
+const imgbbUploader = require('imgbb-uploader');
+
+const IMGBB_API_KEY = process.env.IMGBB_TOKEN;
+
+class ImageReporter {
+  constructor(globalConfig, options) {
+    this._globalConfig = globalConfig;
+    this._options = options;
+  }
+
+  onTestResult(test, testResult, aggregateResults) {
+    if (testResult.numFailingTests && testResult.failureMessage.match(/different from snapshot/)) {
+      const files = fs.readdirSync('./__tests__/__image_snapshots__/__diff_output__/');
+      files.forEach((value) => {
+
+        imgbbUploader(IMGBB_API_KEY, `./__tests__/__image_snapshots__/__diff_output__/${value}`)
+          .then(response => {
+            console.log(chalk.red.bold(`Uploaded image diff file to ${response.url}`));
+          })
+          .catch(error => console.log(error, error.stack))
+      });
+    }
+  }
+}
+
+module.exports = ImageReporter;
