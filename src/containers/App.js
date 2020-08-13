795a453b8c8dd2325ba9adf1a74cd130be8a689e
Joshua Melville <joshua.melville@northwestern.edu>
2020-08-13 11:49:24 +0100

    ensure update info dialog only shows once

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,7 @@ import cx from 'classnames';
 import { Button } from '@codaco/ui';
 import 'swiper/css/swiper.css';
 import { actionCreators as deviceSettingsActions } from '../ducks/modules/deviceSettings';
+import { actionCreators as uiActions } from '../ducks/modules/ui';
 import { actionCreators as dialogActions } from '../ducks/modules/dialogs';
 import '../styles/main.scss';
 import { isElectron, isCordova, isWindows, isMacOS, isLinux, isPreview, getEnv } from '../utils/Environment';
@@ -69,38 +70,42 @@ class App extends PureComponent {
   }
 
   render() {
-    const { children } = this.props;
+    const { children, updateInfoShown, setUpdateInfoShown } = this.props;
 
-    this.props.openDialog({
-      type: 'Notice',
-      title: 'Please upgrade to continue receiving support',
-      canCancel: false,
-      message: (
-        <React.Fragment>
-          <p>
-            Our initial development period has come to an end, and we are pleased to announce
-            the release of the first stable versions of Network Canvas, Architect,
-            and Server. Since stable versions are now available, the version of the software
-            that you are currently using is no longer supported. Please visit our
-            documentation website for information about how to update to the new versions.
-          </p>
-          <p>
-            <Button
-              color="sea-serpent"
-              onClick={() => openExternalLink('https://documentation.networkcanvas.com/docs/technical-documentation/updating-from-beta/')}
-            >
-              Visit documentation website
-            </Button>
-          </p>
-          <p>
-            In the meantime, you can continue to use this version of the software
-            in order to export data, or conclude any in-progress work. The stable releases include
-            many new fixes and features collected from feedback you have provided, and we strongly
-            encourage you to update to them as soon as possible.
-          </p>
-        </React.Fragment>
-      ),
-    });
+    if (!updateInfoShown) {
+      setUpdateInfoShown(true);
+
+      this.props.openDialog({
+        type: 'Notice',
+        title: 'Please upgrade to continue receiving support',
+        canCancel: false,
+        message: (
+          <React.Fragment>
+            <p>
+              Our initial development period has come to an end, and we are pleased to announce
+              the release of the first stable versions of Network Canvas, Architect,
+              and Server. Since stable versions are now available, the version of the software
+              that you are currently using is no longer supported. Please visit our
+              documentation website for information about how to update to the new versions.
+            </p>
+            <p>
+              <Button
+                color="sea-serpent"
+                onClick={() => openExternalLink('https://documentation.networkcanvas.com/docs/technical-documentation/updating-from-beta/')}
+              >
+                Visit documentation website
+              </Button>
+            </p>
+            <p>
+              In the meantime, you can continue to use this version of the software
+              in order to export data, or conclude any in-progress work. The stable releases include
+              many new fixes and features collected from feedback you have provided, and we strongly
+              encourage you to update to them as soon as possible.
+            </p>
+          </React.Fragment>
+        ),
+      });
+    }
 
     return (
       <div className={cx({
@@ -136,15 +141,19 @@ App.propTypes = {
   startFullScreen: PropTypes.bool.isRequired,
   setStartFullScreen: PropTypes.func.isRequired,
   openDialog: PropTypes.func.isRequired,
+  updateInfoShown: PropTypes.bool,
+  setUpdateInfoShown: PropTypes.func.isRequired,
 };
 
 App.defaultProps = {
   children: null,
+  updateInfoShown: false,
 };
 
 const mapDispatchToProps = dispatch => ({
   setStartFullScreen: value => dispatch(deviceSettingsActions.setSetting('startFullScreen', value)),
   openDialog: config => dispatch(dialogActions.openDialog(config)),
+  setUpdateInfoShown: shown => dispatch(uiActions.update({ updateInfoShown: shown })),
 });
 
 function mapStateToProps(state) {
@@ -152,6 +161,7 @@ function mapStateToProps(state) {
     interfaceScale: state.deviceSettings.interfaceScale,
     useDynamicScaling: state.deviceSettings.useDynamicScaling,
     startFullScreen: state.deviceSettings.startFullScreen,
+    updateInfoShown: state.ui.updateInfoShown,
   };
 }
 
