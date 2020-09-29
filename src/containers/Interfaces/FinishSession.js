b8586ad37cfee3b4efb2bd1f20abc7e981da6f5d
Joshua Melville <joshua.melville@northwestern.edu>
2020-09-29 23:51:36 +0100

    tweak form styles

--- src/containers/Interfaces/FinishSession.js
+++ src/containers/Interfaces/FinishSession.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Button } from '@codaco/ui';
 import { actionCreators as sessionActions } from '../../ducks/modules/session';
-import Scroller from '../../components/Scroller';
 
 const FinishSession = ({ endSession }) => {
   const handleFinishSession = () => {
@@ -16,15 +15,13 @@ const FinishSession = ({ endSession }) => {
         <h1 className="finish-session-interface__title type--title-1">
           Finish Interview
         </h1>
-        <Scroller>
-          <div className="finish-session-interface__section finish-session-interface__section--instructions">
-            <p>
-              You have reached the end of the interview.
-              If you are satisfied with the information you have entered, you may finish the
-              interview now.
-            </p>
-          </div>
-        </Scroller>
+        <div className="finish-session-interface__section finish-session-interface__section--instructions">
+          <p>
+            You have reached the end of the interview.
+            If you are satisfied with the information you have entered, you may finish the
+            interview now.
+          </p>
+        </div>
         <div className="finish-session-interface__section finish-session-interface__section--buttons">
           <Button onClick={handleFinishSession}>
             Finish
