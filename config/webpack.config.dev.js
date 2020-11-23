4e220de1a1c767459ddd0b96b804648d9382f4a6
Joshua Melville <joshua.melville@northwestern.edu>
2020-11-23 14:26:27 +0000

    implement health endpoint and update network-exporters implementation

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -50,6 +50,9 @@ module.exports = {
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebookincubator/create-react-app/issues/343.
   devtool: 'cheap-module-source-map',
+  externals: {
+    "archiver": "require('archiver')"   
+  },
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
