2ef703b7a049d06765285ace2077fcae9ee81321
Joshua Melville <joshua.melville@northwestern.edu>
2021-04-16 12:52:50 +0200

    fix validation tests

--- src/utils/__tests__/Validations.test.js
+++ src/utils/__tests__/Validations.test.js
@@ -51,9 +51,9 @@ describe('Validations', () => {
     const errorMessage = 'Your answer must be 5 characters or more';
     const subject = minLength(5);
 
-    it('passes for null or undefined', () => {
-      expect(subject(null)).toBe(undefined);
-      expect(subject(undefined)).toBe(undefined);
+    it('fails for null or undefined', () => {
+      expect(subject(null)).toBe(errorMessage);
+      expect(subject(undefined)).toBe(errorMessage);
     });
 
     it('fails for a smaller string', () => {
@@ -181,9 +181,9 @@ describe('Validations', () => {
     const errorMessage = 'You must choose a maximum of 2 option(s)';
     const subject = maxSelected(2);
 
-    it('fails for null or undefined', () => {
-      expect(subject(null)).toBe(errorMessage);
-      expect(subject(undefined)).toBe(errorMessage);
+    it('passes for null or undefined', () => {
+      expect(subject(null)).toBe(undefined);
+      expect(subject(undefined)).toBe(undefined);
     });
 
     it('passes for an empty array', () => {
