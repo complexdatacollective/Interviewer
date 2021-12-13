32c08805768110d82963d1fd438f289b90de3301
Steve Mckellar <steve@wwqrd.uk>
2021-12-13 16:25:52 +0000

    Simple start/play button for simulation panel

--- src/components/RealtimeCanvas/SimulationPanel.js
+++ src/components/RealtimeCanvas/SimulationPanel.js
@@ -1,30 +1,10 @@
 /* eslint-disable no-param-reassign */
-import React, {
-  useState,
-  useContext,
-  useCallback,
-} from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
-import { motion, useDragControls } from 'framer-motion';
-import MinimizeIcon from '@material-ui/icons/MinimizeRounded';
+import { motion } from 'framer-motion';
 import PlayIcon from '@material-ui/icons/PlayArrowRounded';
 import PauseIcon from '@material-ui/icons/PauseRounded';
 import LayoutContext from '../../contexts/LayoutContext';
-
-const panelVariants = {
-  minimized: {
-    height: 0,
-    transition: {
-    },
-  },
-  normal: {
-    height: 'auto',
-    transition: {
-      when: 'afterChildren',
-    },
-  },
-};
 
 const PlayPauseButton = ({
   onClick,
@@ -57,15 +37,6 @@ const SimulationPanel = ({
     simulation,
   } = useContext(LayoutContext);
 
-  const [isMinimized, setMinimized] = useState(false);
-  const dragControls = useDragControls();
-
-  const toggleMinimize = () => setMinimized((minimized) => !minimized);
-
-  const handleStartDrag = useCallback((event) => {
-    dragControls.start(event);
-  }, [dragControls]);
-
   if (!allowAutomaticLayout) { return null; }
 
   const {
@@ -77,45 +48,16 @@ const SimulationPanel = ({
     <motion.div
       className="simulation-panel"
       drag
-      dragControls={dragControls}
       dragConstraints={dragConstraints}
-      dragListener={false}
     >
       <motion.div
-        className="simulation-panel__header"
-        onTap={toggleMinimize}
-        onPointerDown={handleStartDrag}
+        className="simulation-panel__control"
+        onTap={toggleSimulation}
       >
-        { isMinimized ? (
-          <motion.div
-            role="button"
-            aria-label="Tap to show simulation controls"
-            className="simulation-panel__header-open-prompt"
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0, transition: { delay: 0.5 } }}
-          >
-            <strong>Tap to show simulation controls</strong>
-          </motion.div>
-        ) : (
-          <MinimizeIcon className="simulation-panel__header-minimize" titleAccess="Minimize" />
-        )}
-      </motion.div>
-      <motion.div
-        className="simulation-panel__controls"
-        animate={isMinimized ? 'minimized' : 'normal'}
-        variants={panelVariants}
-      >
-        <motion.div
-          className={cx(
-            'simulation-panel__enable',
-            { 'simulation-panel__enable--active': simulationEnabled },
-          )}
-        >
-          <PlayPauseButton
-            isPlaying={simulationEnabled}
-            onClick={toggleSimulation}
-          />
-        </motion.div>
+        <div className="simulation-panel__control-icon">
+          {simulationEnabled ? <PauseIcon /> : <PlayIcon />}
+        </div>
+        {simulationEnabled ? 'Pause' : 'Play'}
       </motion.div>
     </motion.div>
   );
