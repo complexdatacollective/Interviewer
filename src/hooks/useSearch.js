af79f424c607178b375c99d144db0925aea844b5
Joshua Melville <joshua.melville@northwestern.edu>
2022-02-04 16:46:12 +0200

    fix issue where roster could be stuck in the searching state

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,12 +3,12 @@ import {
   useState,
   useEffect,
   useRef,
+  useCallback,
 } from 'react';
 import Fuse from 'fuse.js';
-import useDebounce from './useDebounce';
+import { debounce } from 'lodash';
 
 const MIN_QUERY_LENGTH = 1;
-const SEARCH_DELAY = 0;
 const DEBOUNCE_DELAY = 500;
 
 const defaultFuseOptions = {
@@ -20,13 +20,14 @@ const defaultFuseOptions = {
   useExtendedSearch: true,
 };
 
+// JRM: removed - see note in useSearch
 // Variation of useState which includes a debounced value
-const useQuery = (initialQuery, delay = DEBOUNCE_DELAY) => {
-  const [query, setQuery] = useState(initialQuery);
-  const debouncedQuery = useDebounce(query, delay);
+// const useQuery = (initialQuery, delay = DEBOUNCE_DELAY) => {
+//   const [query, setQuery] = useState(initialQuery);
+//   const debouncedQuery = useDebounce(query, delay);
 
-  return [debouncedQuery, setQuery, query];
-};
+//   return [debouncedQuery, setQuery, query];
+// };
 
 /**
  * useSearch
@@ -48,7 +49,21 @@ const useQuery = (initialQuery, delay = DEBOUNCE_DELAY) => {
  */
 const useSearch = (list, options, initialQuery = '') => {
   const delayRef = useRef();
-  const [query, setQuery, displayQuery] = useQuery(initialQuery);
+  /**
+   * TODO: This is not what was intended.
+   *
+   * The initial implementation debounced the query, but this just created
+   * an artificial delay to show a loading screen. What we wanted was to
+   * show a loading screen _if necessary_ while search results came back.
+   *
+   * The correct way to implement this is going to be to move fuse to a
+   * webworker, and develop a messaging system for data and results.
+   *
+   * This could be done as part of pre-processing assets at the start of
+   * the interview. A worker could be created for each network asset,
+   * which optionally exposes search and sort methods
+   */
+  const [query, setQuery] = useState(initialQuery);
   const [results, setResults] = useState(list);
   const [isWaiting, setIsWaiting] = useState(false);
 
@@ -58,7 +73,6 @@ const useSearch = (list, options, initialQuery = '') => {
 
   const search = (_query) => {
     clearTimeout(delayRef.current);
-    const startTime = new Date();
     const fuse = new Fuse(list, fuseOptions);
     const res = fuse.search(_query);
 
@@ -67,20 +81,9 @@ const useSearch = (list, options, initialQuery = '') => {
       relevance: 1 - score, // fuseJS relevance is reverse nomalized (0 is perfect match)
     }));
 
-    const endTime = new Date();
-    const delay = SEARCH_DELAY - (endTime - startTime);
-
     if (list.length < 100) {
       setResults(r);
       setIsWaiting(false);
-      return;
-    }
-
-    if (delay > 0) {
-      delayRef.current = setTimeout(() => {
-        setResults(r);
-        setIsWaiting(false);
-      }, delay);
       return;
     }
 
@@ -89,23 +92,29 @@ const useSearch = (list, options, initialQuery = '') => {
   };
 
   useEffect(() => {
-    if (displayQuery.length < MIN_QUERY_LENGTH) { return; }
+    if (query.length < MIN_QUERY_LENGTH) { return; }
     setIsWaiting(true);
-  }, [displayQuery]);
+  }, [query]);
+
+  const debouncedSearch = useCallback(
+    debounce(search, DEBOUNCE_DELAY),
+    [],
+  );
 
   useEffect(() => {
     if (!hasQuery) {
+      setIsWaiting(false);
       return;
     }
 
-    search(query);
+    debouncedSearch(query);
   }, [query]);
 
   const returnResults = useMemo(() => (
     hasQuery ? results : list
   ), [hasQuery, list, results]);
 
-  return [returnResults, displayQuery, setQuery, isWaiting, hasQuery];
+  return [returnResults, query, setQuery, isWaiting, hasQuery];
 };
 
 export default useSearch;
