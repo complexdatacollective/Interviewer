f92561e2f321a99e39df946d83858c87536f7f34
Rebecca Madsen <sollasollew@outlook.com>
2022-03-09 21:46:52 -0500

    update schema and networkQuery

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const PROTOCOL_EXTENSION = '.netcanvas';
 
 // Target protocol schema version. Used to determine compatibility & migration
-const APP_SCHEMA_VERSION = 6;
+const APP_SCHEMA_VERSION = 7;
 
 const DEVICE_API_VERSION = '2'; // X-Device-API-Version
 
-const APP_SUPPORTED_SCHEMA_VERSIONS = [4, 5, 6];
+const APP_SUPPORTED_SCHEMA_VERSIONS = [4, 5, 6, 7];
 
 const DEVELOPMENT_PROTOCOL_URL_V4 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20201207104100-8547e9b/Development.netcanvas';
 const DEVELOPMENT_PROTOCOL_URL_V5 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20210416130716-c6bbb73/Development.netcanvas';
