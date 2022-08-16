27ddbd982c102b7de42e338ed96b124e01d93285
Joshua Melville <joshua.melville@northwestern.edu>
2022-08-16 15:23:55 +0200

    backport slow alter form fix

--- src/containers/SlidesForm/SlidesForm.js
+++ src/containers/SlidesForm/SlidesForm.js
@@ -1,28 +1,38 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React, {
   useState,
   useEffect,
   useRef,
   useCallback,
 } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import cx from 'classnames';
 import uuid from 'uuid';
 import PropTypes from 'prop-types';
 import { debounce } from 'lodash';
-import { connect } from 'react-redux';
+import { connect, useDispatch } from 'react-redux';
 import { ProgressBar } from '@codaco/ui';
 import { Markdown } from '@codaco/ui/lib/components/Fields';
 import { submit, isValid, isDirty } from 'redux-form';
-import Swiper from 'react-id-swiper';
-import { getCSSVariableAsNumber } from '@codaco/ui/lib/utils/CSSVariables';
 import { actionCreators as dialogActions } from '../../ducks/modules/dialogs';
 import useReadyForNextStage from '../../hooks/useReadyForNextStage';
 
 const confirmDialog = {
   type: 'Confirm',
   title: 'Discard changes?',
-  message: 'This form contains invalid data, so it cannot be saved. If you continue it will be reset and your changes will be lost. Do you want to discard your changes?',
+  message: 'This form contains invalid data, so it cannot be saved. If you continue it will be reset, and your changes will be lost. Do you want to discard your changes?',
   confirmLabel: 'Discard changes',
+};
+
+const slideVariants = {
+  show: {
+    y: 0,
+  },
+  hideTop: {
+    y: '-100%',
+  },
+  hideBottom: {
+    y: '100%',
+  },
 };
 
 const SlidesForm = (props) => {
@@ -31,84 +41,60 @@ const SlidesForm = (props) => {
     stage,
     items,
     slideForm: SlideForm,
-    submitFormRedux,
     parentClass,
     registerBeforeNext,
     onComplete,
-    openDialog,
     getFormName,
     isFormValid,
     isFormDirty,
     updateItem,
   } = props;
 
-  const formState = useRef({
-    isFormValid,
-    isFormDirty,
-  });
+  const dispatch = useDispatch();
+  const openDialog = (dialog) => dispatch(dialogActions.openDialog(dialog));
+  const submitFormRedux = (formName) => dispatch(submit(formName));
 
+  const [activeIndex, setActiveIndex] = useState(0);
   const [scrollProgress, setScrollProgress] = useState(0);
   const [, setIsReadyForNext] = useReadyForNextStage();
 
   const [pendingDirection, setPendingDirection] = useState(null);
   const [pendingStage, setPendingStage] = useState(-1);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [swiper, updateSwiper] = useState(null);
 
   const getItemIndex = () => activeIndex - 1;
-
   const isIntroScreen = () => activeIndex === 0;
-
-  // Determine if we should leave the stage
-  const shouldContinue = (direction) => {
-    if (items.length === 0) { return true; }
-    if (isIntroScreen() && direction < 0) { return true; }
-    return false;
-  };
-
-  const previousItem = () => {
-    setActiveIndex(getItemIndex());
-    swiper.slidePrev();
-  };
-
-  const nextItem = () => {
-    setActiveIndex(activeIndex + 1);
-    swiper.slideNext();
-  };
-
   const isLastItem = () => activeIndex >= items.length;
 
-  const submitForm = () => {
-    submitFormRedux(getFormName(getItemIndex()));
-  };
+  const previousItem = () => setActiveIndex(getItemIndex());
+  const nextItem = () => setActiveIndex(activeIndex + 1);
 
-  const handleConfirmBack = (confirmed) => {
-    if (confirmed) {
-      previousItem();
-      return;
-    }
+  // Submit the form of whatever slide is currently active.
+  // Get the form name based on the index of the slide.
+  const submitCurrentForm = () => submitFormRedux(getFormName(getItemIndex()));
 
-    submitForm();
-  };
+  // Ref to hold the current slide form state
+  const formState = useRef({
+    isFormValid,
+    isFormDirty,
+  });
 
-  const getIsFormValid = () => (
-    formState.current.isFormValid[getItemIndex()]
-  );
+  // Helpers for accessing form state
+  const getIsFormValid = () => (formState.current.isFormValid[getItemIndex()]);
+  const getIsFormDirty = () => (formState.current.isFormDirty[getItemIndex()]);
 
-  const getIsFormDirty = () => (
-    formState.current.isFormDirty[getItemIndex()]
-  );
-
+  // Update the navigation button to glow when the current form is valid
+  // And we are scrolled to the bottom.
   useEffect(() => {
     formState.current = {
       isFormValid,
       isFormDirty,
     };
 
-    const nextIsReady = getIsFormValid() && scrollProgress === 1;
-    setIsReadyForNext(nextIsReady);
+    const readyForNext = getIsFormValid() && scrollProgress === 1;
+    setIsReadyForNext(readyForNext);
   }, [...isFormValid, ...isFormDirty, setIsReadyForNext, scrollProgress]);
 
+  // Show a confirmation dialog if the form is dirty and invalid.
   const confirmIfChanged = () => {
     if (!getIsFormDirty()) { return Promise.resolve(true); }
     return openDialog(confirmDialog);
@@ -122,70 +108,71 @@ const SlidesForm = (props) => {
    * called, which allows async events to happen such as form submission.
    */
   const beforeNext = (direction, index = -1) => {
+    setPendingDirection(direction);
     const isPendingStageChange = (index !== -1);
+
+    // If we are about to change stage...
     if (isPendingStageChange) {
+      // ... if the form has been changed and is valid, submit  it
       if (getIsFormValid() && getIsFormDirty()) {
         setPendingStage(index);
-        submitForm(); // submit and handleUpdate will complete
+        submitCurrentForm(); // submit and handleUpdate will complete
       } else {
+        // ... otherwise, prompt the user to confirm lost changes
         confirmIfChanged()
           .then((confirmed) => {
             if (confirmed) {
               onComplete(direction); // show next stage and lose changes
               return;
             }
-            submitForm(); // submit so errors will display
+            submitCurrentForm(); // submit so errors will display
           });
       }
       return;
     }
 
     setPendingStage(index);
-    // Determine if we should leave the stage.
-    if (shouldContinue(direction)) {
+
+    // Leave the stage if there are no items or if we are on the intro and
+    // moving backwards
+    if (items.length === 0 || (isIntroScreen() && direction === -1)) {
       onComplete(direction);
       return;
     }
 
-    if (isIntroScreen() && direction > 0) {
+    // If we are on the intro and moving forwards, move to the next item
+    if (isIntroScreen() && direction === 1) {
       nextItem();
       return;
     }
 
-    if (direction < 0 && !getIsFormValid()) {
+    // If we are moving backward and the current form is not valid...
+    if (direction === -1 && !getIsFormValid()) {
       confirmIfChanged()
-        .then(handleConfirmBack);
+        .then((confirmed) => {
+          if (confirmed) {
+            previousItem();
+            return;
+          }
+
+          submitCurrentForm();
+        });
+
       return;
     }
 
     setPendingStage(index);
-    setPendingDirection(direction);
-    submitForm();
+    submitCurrentForm();
   };
-
-  const swiperParams = {
-    direction: 'vertical',
-    containerClass: 'alter-form__swiper swiper-container',
-    speed: getCSSVariableAsNumber('--animation-duration-slow-ms'),
-    slidesPerView: 'auto',
-    centeredSlides: true,
-  };
-
-  const progressClasses = cx(
-    'progress-container',
-    {
-      'progress-container--show': activeIndex > 0,
-    },
-  );
 
   const parentClasses = cx(
-    'alter-form swiper-no-swiping',
+    'alter-form',
     parentClass,
   );
 
   const isComplete = (direction, stageIndex) => {
-    if (isIntroScreen() && direction < 0) { return true; }
-    if (isLastItem() && direction > 0) { return true; }
+    if (isIntroScreen() && direction === -1) { return true; }
+    if (isLastItem() && direction === 1) { return true; }
     if (stageIndex !== -1) { return true; }
     return false;
   };
@@ -209,7 +196,7 @@ const SlidesForm = (props) => {
       return;
     }
 
-    if (pendingDirection < 0) {
+    if (pendingDirection === -1) {
       previousItem();
       return;
     }
@@ -219,7 +206,7 @@ const SlidesForm = (props) => {
 
   useEffect(() => {
     registerBeforeNext(beforeNext);
-  }, [swiper, beforeNext]);
+  }, [beforeNext]);
 
   // enter key should always move forward, and needs to process using beforeNext
   const handleEnterSubmit = (e) => {
@@ -227,48 +214,86 @@ const SlidesForm = (props) => {
     e.preventDefault();
   };
 
+  const renderActiveSlide = useCallback(() => {
+    const itemIndex = getItemIndex();
+    const formName = getFormName(itemIndex);
+
+    const slideForm = {
+      ...form,
+      form: formName,
+    };
+
+    return (
+      <motion.div
+        key={itemIndex}
+        className="slide-wrapper"
+        variants={slideVariants}
+        animate="show"
+        initial={pendingDirection === 1 ? 'hideBottom' : 'hideTop'}
+        exit={pendingDirection === 1 ? 'hideTop' : 'hideBottom'}
+        transition={{ easing: 'easeInOutQuad', duration: 0.5 }}
+      >
+        <SlideForm
+          key={itemIndex}
+          subject={stage.subject}
+          item={items[itemIndex]}
+          onUpdate={handleUpdate}
+          onScroll={handleScroll}
+          form={slideForm}
+          submitButton={<button type="submit" key="submit" aria-label="Submit" hidden onClick={handleEnterSubmit} />}
+        />
+      </motion.div>
+    );
+  }, [activeIndex, items, getFormName, form, stage.subject, pendingDirection]);
+
+  const renderIntroSlide = useCallback(() => (
+    <motion.div
+      key="introduction-wrapper"
+      className="introduction-wrapper"
+      variants={slideVariants}
+      animate="show"
+      initial={pendingDirection === 1 ? 'hideBottom' : 'hideTop'}
+      exit="hideTop"
+      transition={{ easing: 'easeInOutQuad', duration: 0.5 }}
+    >
+      <div
+        className="alter-form__introduction"
+      >
+        <h1>{stage.introductionPanel.title}</h1>
+        <Markdown
+          label={stage.introductionPanel.text}
+        />
+      </div>
+    </motion.div>
+  ), [activeIndex, items, getFormName, form, stage.subject, pendingDirection]);
+
   return (
     <div className={parentClasses}>
-      <Swiper {...swiperParams} getSwiper={updateSwiper}>
-        <div>
-          <div key="alter-form__introduction" className="slide-content alter-form__introduction">
-            <h1>{stage.introductionPanel.title}</h1>
-            <Markdown
-              label={stage.introductionPanel.text}
-            />
-          </div>
-        </div>
-
-        {items.map((item, itemIndex) => {
-          const formName = getFormName(itemIndex);
-          const slideForm = {
-            ...form,
-            form: formName,
-          };
-
-          return (
-            <SlideForm
-              key={itemIndex}
-              subject={stage.subject}
-              item={item}
-              onUpdate={handleUpdate}
-              onScroll={handleScroll}
-              form={slideForm}
-              submitButton={<button type="submit" key="submit" aria-label="Submit" hidden onClick={handleEnterSubmit} />}
-            />
-          );
-        })}
-      </Swiper>
-      <div className={progressClasses}>
-        <h6 className="progress-container__status-text">
-          <strong>{activeIndex}</strong>
-          {' '}
-          of
-          {' '}
-          <strong>{items.length}</strong>
-        </h6>
-        <ProgressBar orientation="horizontal" percentProgress={(activeIndex / items.length) * 100} nudge={false} />
+      <div className="slide-container">
+        <AnimatePresence initial={false}>
+          { isIntroScreen() ? renderIntroSlide() : renderActiveSlide() }
+        </AnimatePresence>
       </div>
+      <AnimatePresence>
+        { !isIntroScreen() && (
+          <motion.div
+            className="progress-container"
+            key="progress-container"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 0.5, duration: 0.5 } }}
+            exit={{ opacity: 0, y: 100 }}
+          >
+            <h6 className="progress-container__status-text">
+              <strong>{activeIndex}</strong>
+              {' '}
+              of
+              {' '}
+              <strong>{items.length}</strong>
+            </h6>
+            <ProgressBar orientation="horizontal" percentProgress={(activeIndex / items.length) * 100} nudge={false} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
@@ -277,20 +302,14 @@ SlidesForm.propTypes = {
   form: PropTypes.object.isRequired,
   stage: PropTypes.object.isRequired,
   items: PropTypes.array,
-  stageIndex: PropTypes.number.isRequired,
-  submitFormRedux: PropTypes.func.isRequired,
   updateItem: PropTypes.func.isRequired,
-  className: PropTypes.string,
   parentClass: PropTypes.string,
-  itemName: PropTypes.string,
   slideForm: PropTypes.elementType.isRequired,
 };
 
 SlidesForm.defaultProps = {
   items: [],
-  className: '',
   parentClass: '',
-  itemName: '',
 };
 
 const makeMapStateToProps = () => {
@@ -316,12 +335,7 @@ const makeMapStateToProps = () => {
   };
 };
 
-const mapDispatchToProps = {
-  submitFormRedux: submit,
-  openDialog: dialogActions.openDialog,
-};
-
-const withStore = connect(makeMapStateToProps, mapDispatchToProps);
+const withStore = connect(makeMapStateToProps);
 
 export { SlidesForm };
 
