d010e684f62e40a6cfcea07d34b75cfc9d4c631e
Joshua Melville <joshua.melville@northwestern.edu>
2022-09-14 08:28:27 +0200

    correct development protocol case

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const DEVELOPMENT_PROTOCOL_URL_V4 = 'https://github.com/complexdatacollective/de
 const DEVELOPMENT_PROTOCOL_URL_V5 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20210416130716-c6bbb73/Development.netcanvas';
 const DEVELOPMENT_PROTOCOL_URL_V6 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220130211710-1908698/Development.netcanvas';
 const DEVELOPMENT_PROTOCOL_URL_V7 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220311112556-19d1939/Development.netcanvas';
-const DEVELOPMENT_PROTOCOL_URL_v8 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220914054719-b7281b1/Development.netcanvas';
+const DEVELOPMENT_PROTOCOL_URL_V8 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220914054719-b7281b1/Development.netcanvas';
 
 // Cannonical for this version
 const DEVELOPMENT_PROTOCOL_URL = DEVELOPMENT_PROTOCOL_URL_V8;
