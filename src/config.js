75ce4a3eb26d61cadf010100d01eb5bb01fb414b
Joshua Melville <joshua.melville@northwestern.edu>
2022-09-14 07:54:47 +0200

    update networkQuery ref and development protocol schema 8

--- src/config.js
+++ src/config.js
@@ -11,9 +11,10 @@ const DEVELOPMENT_PROTOCOL_URL_V4 = 'https://github.com/complexdatacollective/de
 const DEVELOPMENT_PROTOCOL_URL_V5 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20210416130716-c6bbb73/Development.netcanvas';
 const DEVELOPMENT_PROTOCOL_URL_V6 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220130211710-1908698/Development.netcanvas';
 const DEVELOPMENT_PROTOCOL_URL_V7 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220311112556-19d1939/Development.netcanvas';
+const DEVELOPMENT_PROTOCOL_URL_v8 = 'https://github.com/complexdatacollective/development-protocol/releases/download/20220914054719-b7281b1/Development.netcanvas';
 
 // Cannonical for this version
-const DEVELOPMENT_PROTOCOL_URL = DEVELOPMENT_PROTOCOL_URL_V7;
+const DEVELOPMENT_PROTOCOL_URL = DEVELOPMENT_PROTOCOL_URL_V8;
 
 module.exports = {
   PROTOCOL_EXTENSION,
@@ -24,5 +25,6 @@ module.exports = {
   DEVELOPMENT_PROTOCOL_URL_V5,
   DEVELOPMENT_PROTOCOL_URL_V6,
   DEVELOPMENT_PROTOCOL_URL_V7,
+  DEVELOPMENT_PROTOCOL_URL_V8,
   DEVICE_API_VERSION,
 };
