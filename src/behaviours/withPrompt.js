a197e499926650c97d6e1f3e6dc747deb012321d
Joshua Melville <joshua.melville@northwestern.edu>
2023-04-11 14:51:50 +0200

    update  to automatically process prompt sort rules to use the new format

--- src/behaviours/withPrompt.js
+++ src/behaviours/withPrompt.js
@@ -4,13 +4,43 @@ import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { actionCreators as sessionsActions } from '../ducks/modules/sessions';
 import { getPromptIndexForCurrentSession } from '../selectors/session';
-import { getProtocolStages } from '../selectors/protocol';
+import { getAllVariableUUIDsByEntity, getProtocolStages } from '../selectors/protocol';
 import { get } from '../utils/lodash-replacements';
+import { processProtocolSortRule } from '../utils/createSorter';
+
+/**
+ * Convert sort rules to new format. See `processProtocolSortRule` for details.
+ * @param {Array} prompts
+ * @param {Object} codebookVariables
+ * @returns {Array}
+ * @private
+  */
+const processSortRules = (prompts, codebookVariables) => {
+  const sortProperties = ['bucketSortOrder', 'binSortOrder'];
+
+  return prompts.map((prompt) => {
+    const sortOptions = {};
+    sortProperties.forEach((property) => {
+      const sortRules = get(prompt, property, []);
+      sortOptions[property] = sortRules.map(processProtocolSortRule(codebookVariables));
+    });
+    return {
+      ...prompt,
+      ...sortOptions,
+    };
+  });
+};
 
 export default function withPrompt(WrappedComponent) {
   class WithPrompt extends Component {
     get prompts() {
-      return get(this.props, ['stage', 'prompts']);
+      const {
+        codebookVariables,
+      } = this.props;
+
+      const prompts = get(this.props, ['stage', 'prompts'], []);
+      const processedPrompts = processSortRules(prompts, codebookVariables);
+      return processedPrompts;
     }
 
     get promptsCount() {
@@ -51,7 +81,8 @@ export default function withPrompt(WrappedComponent) {
     }
 
     render() {
-      const { promptIndex, ...rest } = this.props;
+      const { promptIndex, codebookVariables, ...rest } = this.props;
+
       return (
         <WrappedComponent
           prompt={this.prompt()}
@@ -82,6 +113,7 @@ export default function withPrompt(WrappedComponent) {
     return {
       promptIndex,
       stage: ownProps.stage || getProtocolStages(state)[ownProps.stageIndex],
+      codebookVariables: getAllVariableUUIDsByEntity(state),
     };
   }
 
