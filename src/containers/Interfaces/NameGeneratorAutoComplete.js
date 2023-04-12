6e99521800ed4c0851d8dacc36e89d936eef907d
Joshua Melville <joshua.melville@northwestern.edu>
2023-04-12 17:32:08 +0200

    pass stage to node list on namegen autocomplete - fixes crash

--- src/containers/Interfaces/NameGeneratorAutoComplete.js
+++ src/containers/Interfaces/NameGeneratorAutoComplete.js
@@ -84,6 +84,7 @@ class NameGeneratorAutoComplete extends Component {
             id={ListId}
             listId={`${stage.id}_${prompt.id}_${ListId}`}
             items={nodesForPrompt}
+            stage={stage}
             itemType="EXISTING_NODE"
           />
         </div>
