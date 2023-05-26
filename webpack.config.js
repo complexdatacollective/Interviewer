15407c52fe4fb03bb7eb336080ce392d18de28ed
Joshua Melville <joshua.melville@northwestern.edu>
2023-05-26 13:23:27 +0200

    remove webpack and add babel

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+};
